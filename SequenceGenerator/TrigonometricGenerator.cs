4541dc0cf2538c1f779fe1b70d0d6dd6e44b4c57
Marc Fangmeyer <marc.fangmeyer@gmx.de>
2018-07-25 19:42:13 +0200

    Ctor and defaults in TrigonometricGenerator class

--- SequenceGenerator/TrigonometricGenerator.cs
+++ SequenceGenerator/TrigonometricGenerator.cs
@@ -7,6 +7,8 @@ namespace SequenceGenerator
     public class TrigonometricGenerator : ISequenceGenerator<int>
     {
         private bool _run;
+        private double _stepWidth;
+        private double _timeSpan;
 
         public IObservable<int> DataStream { get; private set; }
 
@@ -20,11 +22,18 @@ namespace SequenceGenerator
             _run = false;
         }
 
+        public TrigonometricGenerator()
+        {
+            _timeSpan = 10;
+            _stepWidth = 0.01;
+            DataStream = InitInnerObservable();
+        }
+
         private IObservable<int> InitInnerObservable()
         {
-            return Observable.Interval(TimeSpan.FromMilliseconds(10), new EventLoopScheduler())
+            return Observable.Interval(TimeSpan.FromMilliseconds(_timeSpan), new EventLoopScheduler())
                              .Where(s => _run)
-                             .Scan(0d, (acc, source) => (acc + 0.01))
+                             .Scan(0d, (acc, source) => (acc + _stepWidth))
                              .Select(x => (int)Math.Sin(x));
         }
     }
