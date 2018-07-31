22f3ea7c1bc8a4a357893c56d3e51aef265822ae
Marc Fangmeyer <marc.fangmeyer@gmx.de>
2018-07-31 20:30:12 +0200

    Changed experimental animated frame time test to MVVM pattern

--- CapFrameX/MainWindow.xaml.cs
+++ CapFrameX/MainWindow.xaml.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Reactive.Linq;
-using System.Threading;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Media;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
 namespace CapFrameX
 {
@@ -13,85 +7,10 @@ namespace CapFrameX
     /// </summary>
     public partial class MainWindow : Window
     {
-        private int _stepWidth;
-        private int _refreshRate;
-        private int _xPos;
-        private IDisposable _disposableSequence;
-
         public MainWindow()
         {
             InitializeComponent();
-        }
-
-        private void Button_Click_Start(object sender, RoutedEventArgs e)
-        {
-            _stepWidth = 2;
-            _refreshRate = 6;
-            _xPos = 2;
-
-            var context = SynchronizationContext.Current;
-
-            _disposableSequence?.Dispose();
-            _disposableSequence = Observable.Generate(
-                                    0, // initialState
-                                    x => true, //condition
-                                    x => x, //iterate
-                                    x => x, //resultSelector
-                                    x => TimeSpan.FromMilliseconds(_refreshRate))
-                                //.ObserveOn(Scheduler.Default)
-                                .ObserveOn(context)
-                                .SubscribeOn(context)
-                                .Subscribe(x => UpdateRectangle());
-        }
-
-        private void UpdateRectangle()
-        {
-            FrameTestCanvas.Children.Clear();
-
-            Point p = new Point(_xPos, 100);
-
-            // Initialize a new Rectangle
-            Rectangle r = new Rectangle
-            {
-                // Set up rectangle's size
-                Width = 100,
-                Height = 300,
-
-                // Set up the Background color
-                Fill = Brushes.Black
-            };
-
-            // Set up the position in the canvas control
-            Canvas.SetTop(r, p.Y);
-            Canvas.SetLeft(r, p.X);
-
-            FrameTestCanvas.Children.Add(r);
-
-            _xPos += _stepWidth;
-
-            if (_xPos > FrameTestCanvas.ActualWidth - 120)
-                _xPos = 100;
-        }
-
-        private void Button_Click_End(object sender, RoutedEventArgs e)
-        {
-            _disposableSequence?.Dispose();
-            FrameTestCanvas.Children.Clear();
-        }
-
-        private void Slider_Stepwidth_ValueChanged(object sender,
-            RoutedPropertyChangedEventArgs<double> e)
-        {
-            var slider = sender as Slider;
-            _stepWidth = (int)slider.Value;
-        }
-
-        private void Slider_Refreshrate_ValueChanged(object sender,
-          RoutedPropertyChangedEventArgs<double> e)
-        {
-            var slider = sender as Slider;
-            _refreshRate = (int)slider.Value;
-            FpsCounterTextBox.Text = Math.Round(1000d / _refreshRate, 0).ToString();
+            DataContext = new MainViewModel(this);
         }
     }
 }
--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # CapFrameX
-Frame capture analysis tool (in the process of developing)
+Frame capture analysis tool (in process of developing)
 
 # Dev Roadmap
 * Experimental animated frame time test (work in progess)
@@ -20,6 +20,6 @@ What am I gonna do here is to provide a visualization of frame times. This could
 Features:
 * Push various synthetic sequences
 * Push real-world data (data from OCAT)
-* Watch parallel live chart
+* Watch synchronous live chart
 * Watching smoothness/stuttering and aliasing effects at high-contrast transitions due to low FPS
-* What about SharpDX (D2D)?
+* What about SharpDX (D2D)? Would like to try that...
