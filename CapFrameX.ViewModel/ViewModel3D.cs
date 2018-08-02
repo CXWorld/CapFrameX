9a93ff6e5fa4693b68547e8e50acb45263c32503
Marc Fangmeyer <marc.fangmeyer@gmx.de>
2018-08-02 21:11:01 +0200

    Cleanup and edited README

--- CapFrameX.ViewModel/ViewModel3D.cs
+++ CapFrameX.ViewModel/ViewModel3D.cs
@@ -1,16 +1,16 @@
-﻿using Prism.Mvvm;
+﻿using HelixToolkit.Wpf.SharpDX;
+using Prism.Mvvm;
+using SharpDX;
 using Color = System.Windows.Media.Color;
 using Colors = System.Windows.Media.Colors;
 using Vector3D = System.Windows.Media.Media3D.Vector3D;
-using HelixToolkit.Wpf.SharpDX;
-using SharpDX;
-using System.IO;
 
 namespace CapFrameX.ViewModel
 {
     public class ViewModel3D : BindableBase
     {
         public MeshGeometry3D Model { set; get; }
+
         public PhongMaterial ModelMaterial { set; get; } = PhongMaterials.Black;
 
         public Vector3D Light1Direction { get; set; } = new Vector3D(1, -1, -1);
@@ -25,30 +25,15 @@ namespace CapFrameX.ViewModel
 
         public Color Light3Color { set; get; } = Colors.Green;
 
-        private string NormalTexture = @"TextureCheckerboard2_dot3.jpg";
-
-        private string Texture = @"TextureCheckerboard2.jpg";
-
         public ViewModel3D()
         {
             var builder = new MeshBuilder(true, true, true);
+            // ToDo: Better use 2D Shape?
             builder.AddBox(new Vector3(0, 2.5f, 0), 5, 5, 5);
-            // builder.AddBox(new Vector3(0, 0, 0), 10, 0.1, 10);
             Model = builder.ToMeshGeometry3D();
-            //var diffuseMap = LoadFileToMemory(new System.Uri(Texture, System.UriKind.RelativeOrAbsolute).ToString());
-            //var normalMap = LoadFileToMemory(new System.Uri(NormalTexture, System.UriKind.RelativeOrAbsolute).ToString());
+
             //ModelMaterial.DiffuseMap = diffuseMap;
             //ModelMaterial.NormalMap = normalMap;
-        }
-
-        public static MemoryStream LoadFileToMemory(string filePath)
-        {
-            using (var file = new FileStream(filePath, FileMode.Open))
-            {
-                var memory = new MemoryStream();
-                file.CopyTo(memory);
-                return memory;
-            }
         }
     }
 }
--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 Frame capture analysis tool (in process of developing)
 
 # Dev Roadmap
-* Experimental animated frame time test (work in progess)
+* Experimental 2D/3D animated frame time test (work in progess)
 * Application architecture based on MVVM pattern und DI (work in progess)
 * UI design
 * Advanced statistical methods and algorithm validation
@@ -20,6 +20,7 @@ What am I gonna do here is to provide a visualization of frame times. This could
 Features:
 * Push various synthetic sequences
 * Push real-world data (data from OCAT)
-* Watch synchronous live chart
+* Watch simultaneous live chart
 * Watching smoothness/stuttering and aliasing effects at high-contrast transitions due to low FPS
-* What about SharpDX (D2D)? Would like to try that...
+* 2D/3D animation
+* 3D animation using adaptive sync
