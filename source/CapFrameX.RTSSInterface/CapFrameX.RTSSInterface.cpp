985c750d24e8da91e5868502a766631f73ff1a95
DevTechProfile <mark.fangmeyer@walter-machines.de>
2019-11-21 10:30:16 +0100

    initial project setup for RTSS interface implementation (C++ DLS with CLR support)

---
+++ source/CapFrameX.RTSSInterface/CapFrameX.RTSSInterface.cpp
@@ -0,0 +1,6 @@
+// CapFrameX.RTSSInterface.cpp : Definiert die exportierten Funktionen für die DLL-Anwendung.
+//
+
+#include "stdafx.h"
+
+
---
+++ source/CapFrameX.RTSSInterface/stdafx.h
@@ -0,0 +1,16 @@
+// stdafx.h: Includedatei für Include-Standardsystemdateien
+// oder häufig verwendete projektspezifische Includedateien,
+// die nur in unregelmäßigen Abständen geändert werden.
+//
+
+#pragma once
+
+#include "targetver.h"
+
+#define WIN32_LEAN_AND_MEAN             // Selten verwendete Komponenten aus Windows-Headern ausschließen
+// Windows-Headerdateien
+#include <windows.h>
+
+
+
+// Hier auf zusätzliche Header verweisen, die das Programm benötigt.
