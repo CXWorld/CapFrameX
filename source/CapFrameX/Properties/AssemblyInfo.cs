f7c502f68aca567e92ba9eb53bb39764329b4fed
DevTechProfile <devtechprofile@hotmail.com>
2019-11-27 19:33:37 +0100

    ComboBox popup placement workaround

--- source/CapFrameX/Properties/AssemblyInfo.cs
+++ source/CapFrameX/Properties/AssemblyInfo.cs
@@ -1,6 +1,7 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
 using System.Windows;
+using System.Windows.Media;
 
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
