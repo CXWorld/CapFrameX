3f1acdbe341ce6c37bfaa19a4560d2f559404dff
DevTechProfile <devtechprofile@hotmail.com>
2020-01-02 10:11:48 +0100

    updated year information in AssemblyInfo files

--- source/CapFrameX.Configuration/Properties/AssemblyInfo.cs
+++ source/CapFrameX.Configuration/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("CapFrameX.Configuration")]
-[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
--- source/CapFrameX.Overlay/Properties/AssemblyInfo.cs
+++ source/CapFrameX.Overlay/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("CapFrameX.Overlay")]
-[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
