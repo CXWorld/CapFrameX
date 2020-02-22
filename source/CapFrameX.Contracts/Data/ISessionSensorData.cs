006096947254b1cdd6ec6777560ae765dee99fcf
Taxxor <taxxor90@gmail.com>
2020-02-22 01:40:46 +0100

    additional sensor data

--- source/CapFrameX.Contracts/Data/ISessionSensorData.cs
+++ source/CapFrameX.Contracts/Data/ISessionSensorData.cs
@@ -5,6 +5,8 @@
 		int[] GpuPower { get; set; }
 		int[] GpuTemp { get; set; }
 		int[] GpuUsage { get; set; }
+		int[] CpuUsage { get; set; }
+		int[] CpuMaxThreadUsage { get; set; }
 		bool[] IsInGpuLimit { get; set; }
 		double[] MeasureTime { get; set; }
 		double[] RamUsage { get; set; }
--- source/CapFrameX.Data/Session.cs
+++ source/CapFrameX.Data/Session.cs
@@ -120,6 +120,8 @@ namespace CapFrameX.Data
 	public class SessionSensorData : ISessionSensorData
 	{
 		public double[] MeasureTime { get; set; }
+		public int[] CpuUsage { get; set; }
+		public int[] CpuMaxThreadUsage { get; set; }
 		public int[] GpuUsage { get; set; }
 		public double[] RamUsage { get; set; }
 		public bool[] IsInGpuLimit { get; set; }
