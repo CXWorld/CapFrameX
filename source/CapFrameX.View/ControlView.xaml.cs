183a456c3b7fa4c564f959ec793141421d099ed9
DevTechProfile <devtechprofile@hotmail.com>
2020-03-11 22:11:51 +0100

    Jump into treeview node function, recursive treeview creation

--- source/CapFrameX.View/ControlView.xaml.cs
+++ source/CapFrameX.View/ControlView.xaml.cs
@@ -19,217 +19,315 @@ using System.Windows.Data;
 using Microsoft.WindowsAPICodePack.Dialogs;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Collections.Generic;
 
 namespace CapFrameX.View
 {
-	/// <summary>
-	/// Interaction logic for ControlView.xaml
-	/// </summary>
-	public partial class ControlView : UserControl
-	{
-		private const int SEARCH_REFRESH_DELAY_MS = 100;
-		private readonly CollectionViewSource _recordInfoCollection;
+    /// <summary>
+    /// Interaction logic for ControlView.xaml
+    /// </summary>
+    public partial class ControlView : UserControl
+    {
+        private const int SEARCH_REFRESH_DELAY_MS = 100;
+        private readonly CollectionViewSource _recordInfoCollection;
 
-		public ControlView()
-		{
-			InitializeComponent();
+        public ControlView()
+        {
+            InitializeComponent();
 
-			_recordInfoCollection = (CollectionViewSource)Resources["RecordInfoListKey"];
-			Observable.FromEventPattern<TextChangedEventArgs>(RecordSearchBox, "TextChanged")
-				.Throttle(TimeSpan.FromMilliseconds(SEARCH_REFRESH_DELAY_MS))
-				.ObserveOnDispatcher()
-				.Subscribe(t => _recordInfoCollection.View.Refresh());
+            _recordInfoCollection = (CollectionViewSource)Resources["RecordInfoListKey"];
+            Observable.FromEventPattern<TextChangedEventArgs>(RecordSearchBox, "TextChanged")
+                .Throttle(TimeSpan.FromMilliseconds(SEARCH_REFRESH_DELAY_MS))
+                .ObserveOnDispatcher()
+                .Subscribe(t => _recordInfoCollection.View.Refresh());
 
-			CreateTreeViewRoot();
+            var root = CreateTreeViewRoot();
+            CreateTreeViewRecursive(trvStructure.Items[0] as TreeViewItem);
 
-			// Design time!
-			if (DesignerProperties.GetIsInDesignMode(this))
-			{
-				var appConfiguration = new CapFrameXConfiguration();
-				var loggerFactory = new LoggerFactory();
+            TraverseTreeView(root);
 
-				var recordDirectoryObserver = new RecordDirectoryObserver(appConfiguration,
-					loggerFactory.CreateLogger<RecordDirectoryObserver>());
+            if (_observedTreeViewItem != null)
+                JumpToNode(root, _observedTreeViewItem.Header.ToString());
 
-                DataContext = new ControlViewModel(new RecordDirectoryObserver(appConfiguration,
-					loggerFactory.CreateLogger<RecordDirectoryObserver>()), new EventAggregator(), 
-                    new CapFrameXConfiguration(), new RecordManager(loggerFactory.CreateLogger<RecordManager>(), appConfiguration, recordDirectoryObserver, new AppVersionProvider()));
-			}
+            //if (_observedTreeViewItem != null)
+            //{
+            //    var parentList = new List<TreeViewItem>();
+            //    var currentTreeViewItem = _observedTreeViewItem;
+            //    while (currentTreeViewItem.Parent != null && currentTreeViewItem.Parent is TreeViewItem)
+            //    {
+            //        currentTreeViewItem = currentTreeViewItem.Parent as TreeViewItem;
+            //        parentList.Add(currentTreeViewItem);
+            //    }
 
-			SetSortSettings((DataContext as ControlViewModel).AppConfiguration);
-		}
+            //    parentList.Reverse();
+
+            //    foreach (var item in parentList)
+            //    {
+            //        item.IsExpanded = true;
+            //    }
+
+            //    _observedTreeViewItem.IsSelected = true;
+            //}
+
+            SetSortSettings((DataContext as ControlViewModel).AppConfiguration);
+        }
+
+        void JumpToNode(TreeViewItem tvi, string NodeName)
+        {
+            if (tvi == null)
+                return;
+
+            if (tvi.Header.ToString() == NodeName)
+            {
+                tvi.BringIntoView();
+                tvi.IsSelected = true;
+                return;
+            }
+            else
+                tvi.IsExpanded = false;
+
+            if (tvi.HasItems)
+            {
+                foreach (var item in tvi.Items)
+                {
+                    TreeViewItem temp = item as TreeViewItem;
+                    JumpToNode(temp, NodeName);
+                }
+            }
+        }
+
+        private void TraverseTreeView(TreeViewItem item)
+        {
+            DirectoryInfo expandedDir = null;
+            if (item.Tag is DriveInfo)
+                expandedDir = (item.Tag as DriveInfo).RootDirectory;
+            if (item.Tag is DirectoryInfo)
+                expandedDir = (item.Tag as DirectoryInfo);
+            try
+            {
+                foreach (DirectoryInfo subDir in expandedDir.GetDirectories())
+                {
+                    var subItem = CreateTreeItem(subDir, subDir.ToString());
+
+                    if (subDir.FullName == (DataContext as ControlViewModel).AppConfiguration.ObservedDirectory)
+                    {
+                        _observedTreeViewItem = subItem;
+                        return;
+                    }
+                    TraverseTreeView(subItem);
+                }
+            }
+            catch { }
+        }
+
+        private TreeViewItem _observedTreeViewItem;
+
+        private void CreateTreeViewRecursive(TreeViewItem item)
+        {
+            if ((item.Items.Count == 1) && (item.Items[0] is string))
+            {
+                item.Items.Clear();
+
+                DirectoryInfo expandedDir = null;
+                if (item.Tag is DriveInfo)
+                    expandedDir = (item.Tag as DriveInfo).RootDirectory;
+                if (item.Tag is DirectoryInfo)
+                    expandedDir = (item.Tag as DirectoryInfo);
+                try
+                {
+                    foreach (DirectoryInfo subDir in expandedDir.GetDirectories())
+                    {
+                        var subItem = CreateTreeItem(subDir, subDir.ToString());
+                        item.Items.Add(subItem);
+                        CreateTreeViewRecursive(subItem);
+                    }
+                }
+                catch { }
+            }
+        }
+
+        private void SetSortSettings(IAppConfiguration appConfiguration)
+        {
+            string sortMemberPath = appConfiguration.RecordingListSortMemberPath;
+            var direction = appConfiguration.RecordingListSortDirection.ConvertToEnum<ListSortDirection>();
+            var collectionView = CollectionViewSource.GetDefaultView(RecordDataGrid.ItemsSource);
+
+            collectionView.SortDescriptions.Clear();
+            AddSortColumn(RecordDataGrid, sortMemberPath, direction);
+            AddSortColumnsByMemberPath(RecordDataGrid, direction, sortMemberPath);
+        }
+
+        private void RecordDataGrid_Sorting(object sender, DataGridSortingEventArgs e)
+        {
+            var dataGrid = (DataGrid)sender;
+            var appConfiguration = (DataContext as ControlViewModel).AppConfiguration;
+            var collectionView = CollectionViewSource.GetDefaultView(dataGrid.ItemsSource);
+
+            ListSortDirection direction = ListSortDirection.Ascending;
+            if (collectionView.SortDescriptions.FirstOrDefault().PropertyName == e.Column.SortMemberPath)
+                direction = collectionView.SortDescriptions.FirstOrDefault().Direction ==
+                    ListSortDirection.Descending ? ListSortDirection.Ascending : ListSortDirection.Descending;
+
+            collectionView.SortDescriptions.Clear();
+            AddSortColumn((DataGrid)sender, e.Column.SortMemberPath, direction);
+            AddSortColumnsByMemberPath((DataGrid)sender, direction, e.Column.SortMemberPath);
+
+            appConfiguration.RecordingListSortMemberPath = e.Column.SortMemberPath;
+            appConfiguration.RecordingListSortDirection = direction.ConvertToString();
+
+            e.Handled = true;
+        }
+
+        private void AddSortColumn(DataGrid sender, string sortColumn, ListSortDirection direction)
+        {
+            var collectionView = CollectionViewSource.GetDefaultView(sender.ItemsSource);
+            collectionView.SortDescriptions.Add(new SortDescription(sortColumn, direction));
+
+            foreach (var col in sender.Columns.Where(x => x.SortMemberPath == sortColumn))
+            {
+                col.SortDirection = direction;
+            }
+        }
+
+        private void AddSortColumnsByMemberPath(DataGrid dataGrid, ListSortDirection sortDirection, string sortMemberPath)
+        {
+            if (sortMemberPath == "GameName")
+            {
+                AddSortColumn(dataGrid, "CreationTimestamp", sortDirection);
+            }
+
+        }
+
+        private void RecordInfoListOnFilter(object sender, FilterEventArgs e)
+        {
+            e.FilterCollectionByText<IFileRecordInfo>(RecordSearchBox.Text,
+                                        (record, word) => record.CombinedInfo.NullSafeContains(word, true));
+        }
+
+        private void DataGridRow_MouseDoubleClick(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            (DataContext as ControlViewModel).OnRecordSelectByDoubleClick();
+        }
+
+        private void ScrollViewer_PreviewMouseWheel(object sender, System.Windows.Input.MouseWheelEventArgs e)
+        {
+            ScrollViewer scv = (ScrollViewer)sender;
+            scv.ScrollToVerticalOffset(scv.VerticalOffset - e.Delta / 10);
+            e.Handled = true;
+        }
+
+        private TreeViewItem CreateTreeViewRoot()
+        {
+            trvStructure.Items.Clear();
+            var mainfolderpath = (DataContext as ControlViewModel).AppConfiguration.CaptureRootDirectory;
+            var mainfoldername = new DirectoryInfo(ExtractFullPath(mainfolderpath));
+            var rootNode = CreateTreeItem(mainfoldername, mainfoldername.Name);
+            trvStructure.Items.Add(rootNode);
+            rootNode.IsExpanded = true;
+            return rootNode;
+        }
+
+        public void TreeViewItem_Expanded(object sender, RoutedEventArgs e)
+        {
+            TreeViewItem item = e.Source as TreeViewItem;
+            if ((item.Items.Count == 1) && (item.Items[0] is string))
+            {
+                item.Items.Clear();
+
+                DirectoryInfo expandedDir = null;
+                if (item.Tag is DriveInfo)
+                    expandedDir = (item.Tag as DriveInfo).RootDirectory;
+                if (item.Tag is DirectoryInfo)
+                    expandedDir = (item.Tag as DirectoryInfo);
+                try
+                {
+                    foreach (DirectoryInfo subDir in expandedDir.GetDirectories())
+                    {
+                        var subItem = CreateTreeItem(subDir, subDir.ToString());
+                        item.Items.Add(subItem);
+                        subItem.IsExpanded = true;
+                        if (!(subDir.FullName == (DataContext as ControlViewModel).AppConfiguration.ObservedDirectory))
+                        {
+                            subItem.IsExpanded = false;
+                        }
+                        else
+                        {
+
+                            subItem.Focus();
+                            (subItem.Parent as TreeViewItem).IsExpanded = true;
+
+                        }
+                    }
+                }
+                catch { }
+            }
+        }
+
+        private TreeViewItem CreateTreeItem(object o, string name)
+        {
+            TreeViewItem item = new TreeViewItem
+            {
+                Header = name,
+                Tag = o
+            };
+            item.Items.Add("Loading...");
+            return item;
+        }
+
+        private void TreeViewItem_Selected(object sender, RoutedEventArgs e)
+        {
+            TreeViewItem item = e.Source as TreeViewItem;
+            (DataContext as ControlViewModel).RecordObserver.ObserveDirectory((item.Tag as DirectoryInfo).FullName);
+        }
+
+        private string ExtractFullPath(string path)
+        {
+            if (path.Contains(@"MyDocuments\CapFrameX\Captures"))
+            {
+                var documentFolder = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+                path = Path.Combine(documentFolder, @"CapFrameX\Captures");
+            }
+
+            return path;
+        }
+
+        private void RootFolder_PreviewMouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            var result = (DataContext as ControlViewModel).OnSelectRootFolder();
+            if (result)
+            {
+                CreateTreeViewRoot();
+            }
+        }
 
 
-		private void SetSortSettings(IAppConfiguration appConfiguration)
-		{
-			string sortMemberPath = appConfiguration.RecordingListSortMemberPath;
-			var direction = appConfiguration.RecordingListSortDirection.ConvertToEnum<ListSortDirection>();
-			var collectionView = CollectionViewSource.GetDefaultView(RecordDataGrid.ItemsSource);
+        private void TreeView_PreviewMouseRightButtonDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            TreeViewItem treeViewItem = VisualUpwardSearch(e.OriginalSource as DependencyObject);
 
-			collectionView.SortDescriptions.Clear();
-			AddSortColumn(RecordDataGrid, sortMemberPath, direction);
-			AddSortColumnsByMemberPath(RecordDataGrid, direction, sortMemberPath);
-		}
+            if (treeViewItem != null)
+            {
+                treeViewItem.Focus();
+                e.Handled = true;
+            }
+        }
 
-		private void RecordDataGrid_Sorting(object sender, DataGridSortingEventArgs e)
-		{
-			var dataGrid = (DataGrid)sender;
-			var appConfiguration = (DataContext as ControlViewModel).AppConfiguration;
-			var collectionView = CollectionViewSource.GetDefaultView(dataGrid.ItemsSource);
+        static TreeViewItem VisualUpwardSearch(DependencyObject source)
+        {
+            while (source != null && !(source is TreeViewItem))
+                source = VisualTreeHelper.GetParent(source);
 
-			ListSortDirection direction = ListSortDirection.Ascending;
-			if (collectionView.SortDescriptions.FirstOrDefault().PropertyName == e.Column.SortMemberPath)
-				direction = collectionView.SortDescriptions.FirstOrDefault().Direction ==
-					ListSortDirection.Descending ? ListSortDirection.Ascending : ListSortDirection.Descending;
+            return source as TreeViewItem;
+        }
 
-			collectionView.SortDescriptions.Clear();
-			AddSortColumn((DataGrid)sender, e.Column.SortMemberPath, direction);
-			AddSortColumnsByMemberPath((DataGrid)sender, direction, e.Column.SortMemberPath);
+        private void TextBox_KeyDown(object sender, System.Windows.Input.KeyEventArgs e)
+        {
+            if (e.Key != System.Windows.Input.Key.Enter)
+                return;
 
-			appConfiguration.RecordingListSortMemberPath = e.Column.SortMemberPath;
-			appConfiguration.RecordingListSortDirection = direction.ConvertToString();
-
-			e.Handled = true;
-		}
-
-		private void AddSortColumn(DataGrid sender, string sortColumn, ListSortDirection direction)
-		{
-			var collectionView = CollectionViewSource.GetDefaultView(sender.ItemsSource);
-			collectionView.SortDescriptions.Add(new SortDescription(sortColumn, direction));
-
-			foreach (var col in sender.Columns.Where(x => x.SortMemberPath == sortColumn))
-			{
-				col.SortDirection = direction;
-			}
-		}
-
-		private void AddSortColumnsByMemberPath(DataGrid dataGrid, ListSortDirection sortDirection, string sortMemberPath)
-		{
-			if (sortMemberPath == "GameName")
-			{
-				AddSortColumn(dataGrid, "CreationTimestamp", sortDirection);
-			}
-
-		}
-
-		private void RecordInfoListOnFilter(object sender, FilterEventArgs e)
-		{
-			e.FilterCollectionByText<IFileRecordInfo>(RecordSearchBox.Text,
-										(record, word) => record.CombinedInfo.NullSafeContains(word, true));
-		}
-
-		private void DataGridRow_MouseDoubleClick(object sender, System.Windows.Input.MouseButtonEventArgs e)
-		{
-			(DataContext as ControlViewModel).OnRecordSelectByDoubleClick();
-		}
-		
-		private void ScrollViewer_PreviewMouseWheel(object sender, System.Windows.Input.MouseWheelEventArgs e)
-		{
-			ScrollViewer scv = (ScrollViewer)sender;
-			scv.ScrollToVerticalOffset(scv.VerticalOffset - e.Delta / 10);
-			e.Handled = true;
-		}
-
-		private void CreateTreeViewRoot()
-		{
-			trvStructure.Items.Clear();
-			var mainfolderpath = (DataContext as ControlViewModel).AppConfiguration.CaptureRootDirectory;
-			var mainfoldername = new DirectoryInfo(ExtractFullPath(mainfolderpath));
-			var rootNode = CreateTreeItem(mainfoldername, mainfoldername.Name);
-			trvStructure.Items.Add(rootNode);
-			//rootNode.IsSelected = true;
-			rootNode.IsExpanded = true;
-		}
-
-		public void TreeViewItem_Expanded(object sender, RoutedEventArgs e)
-		{
-			TreeViewItem item = e.Source as TreeViewItem;
-			if ((item.Items.Count == 1) && (item.Items[0] is string))
-			{
-				item.Items.Clear();
-
-				DirectoryInfo expandedDir = null;
-				if (item.Tag is DriveInfo)
-					expandedDir = (item.Tag as DriveInfo).RootDirectory;
-				if (item.Tag is DirectoryInfo)
-					expandedDir = (item.Tag as DirectoryInfo);
-				try
-				{
-					foreach (DirectoryInfo subDir in expandedDir.GetDirectories())
-					{ 
-						item.Items.Add(CreateTreeItem(subDir, subDir.ToString()));
-						item.IsExpanded = true;
-					}					
-				}
-				catch { }
-			}
-		}
-
-		private TreeViewItem CreateTreeItem(object o, string name)
-		{
-			TreeViewItem item = new TreeViewItem
-			{
-				Header = name,
-				Tag = o
-			};
-			item.Items.Add("Loading...");
-			return item;
-		}
-
-		private void TreeViewItem_Selected(object sender, RoutedEventArgs e)
-		{
-			TreeViewItem item = e.Source as TreeViewItem;
-			(DataContext as ControlViewModel).RecordObserver.ObserveDirectory((item.Tag as DirectoryInfo).FullName);
-		}
-
-		private string ExtractFullPath(string path)
-		{
-			if (path.Contains(@"MyDocuments\CapFrameX\Captures"))
-			{
-				var documentFolder = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
-				path = Path.Combine(documentFolder, @"CapFrameX\Captures");
-			}
-
-			return path;
-		}
-
-		private void RootFolder_PreviewMouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
-		{
-			var result = (DataContext as ControlViewModel).OnSelectRootFolder();
-			if(result)
-			{				
-				CreateTreeViewRoot();
-			}
-		}
-
-
-		private void TreeView_PreviewMouseRightButtonDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
-		{
-			TreeViewItem treeViewItem = VisualUpwardSearch(e.OriginalSource as DependencyObject);
-
-			if (treeViewItem != null)
-			{
-				treeViewItem.Focus();
-				e.Handled = true;
-			}
-		}
-
-		static TreeViewItem VisualUpwardSearch(DependencyObject source)
-		{
-			while (source != null && !(source is TreeViewItem))
-				source = VisualTreeHelper.GetParent(source);
-
-			return source as TreeViewItem;
-		}
-
-		private void TextBox_KeyDown(object sender, System.Windows.Input.KeyEventArgs e)
-		{
-			if (e.Key != System.Windows.Input.Key.Enter) 
-				return;
-
-			Keyboard.ClearFocus();
-			(DataContext as ControlViewModel).SaveDescriptions();
-			e.Handled = true;
-		}
-	}
+            Keyboard.ClearFocus();
+            (DataContext as ControlViewModel).SaveDescriptions();
+            e.Handled = true;
+        }
+    }
 }
