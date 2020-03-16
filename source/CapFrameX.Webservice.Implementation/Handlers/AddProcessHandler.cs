5b933c4e9be32265e677ad5aa969480d2aa23724
Taxxor <taxxor90@gmail.com>
2020-03-16 21:01:17 +0100

    fix

--- source/CapFrameX.Webservice.Implementation/Handlers/AddProcessHandler.cs
+++ source/CapFrameX.Webservice.Implementation/Handlers/AddProcessHandler.cs
@@ -8,6 +8,7 @@ using CapFrameX.Webservice.Data.Commands;
 using CapFrameX.Webservice.Data.DTO;
 using CapFrameX.Webservice.Data.Interfaces;
 using MediatR;
+using Microsoft.Extensions.Logging;
 
 namespace CapFrameX.Webservice.Implementation.Handlers
 {
@@ -15,18 +16,24 @@ namespace CapFrameX.Webservice.Implementation.Handlers
 	{
 		private readonly IProcessListService _processListService;
 		private readonly IMapper _mapper;
+		private readonly ILogger<AddProcessHandler> _logger;
 
-		public AddProcessHandler(IProcessListService processListService, IMapper mapper) {
+		public AddProcessHandler(IProcessListService processListService, IMapper mapper, ILogger<AddProcessHandler> logger) {
 			_processListService = processListService;
 			_mapper = mapper;
+			_logger = logger;
 		}
+
 		protected override async Task Handle(AddProcessCommand request, CancellationToken cancellationToken)
 		{
 			var process = _mapper.Map<ProcessListData>(request.Process);
 			try
 			{
 				await _processListService.AddProcess(process);
-			} catch { }
+			} catch (Exception e)
+			{
+				_logger.LogError(e, "Add Process failed for {@process}", process);
+			}
 		}
 	}
 }
