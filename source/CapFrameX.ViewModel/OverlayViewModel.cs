3b4b35fff9b1339f1f7808f5277fe7a2f547b541
DevTechProfile <devtechprofile@hotmail.com>
2020-03-26 10:47:27 +0100

    cleanup

--- source/CapFrameX.ViewModel/OverlayViewModel.cs
+++ source/CapFrameX.ViewModel/OverlayViewModel.cs
@@ -14,10 +14,8 @@ using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
-using System.Reactive;
 using System.Reactive.Linq;
 using System.Reactive.Subjects;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Input;
 
@@ -308,7 +306,8 @@ namespace CapFrameX.ViewModel
 
         public ICommand SaveConfigCommand { get; }
 
-        public bool IsRTSSInstalled { get; }
+        public bool IsRTSSInstalled 
+            => !string.IsNullOrEmpty(RTSSUtils.GetRTSSFullPath());
 
         public IAppConfiguration AppConfiguration => _appConfiguration;
 
@@ -361,7 +360,6 @@ namespace CapFrameX.ViewModel
             SaveConfigCommand = new DelegateCommand(
                 () => _overlayEntryProvider.SaveOverlayEntriesToJson());
 
-            IsRTSSInstalled = !string.IsNullOrEmpty(RTSSUtils.GetRTSSFullPath());
             UpdateHpyerlinkText = "To use the overlay, install the latest" + Environment.NewLine +
                 "RivaTuner  Statistics  Server  (RTSS)";
 
