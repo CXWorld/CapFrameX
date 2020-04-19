c2f28c479ffe19fd98574b3971e6aaf32b8d07f7
Taxxor <taxxor90@gmail.com>
2020-04-19 16:24:51 +0200

    cleanup

--- source/CapFrameX.ViewModel/ComparisonViewModelLabel.cs
+++ source/CapFrameX.ViewModel/ComparisonViewModelLabel.cs
@@ -26,114 +26,26 @@ namespace CapFrameX.ViewModel
 			ComparisonModel.InvalidatePlot(true);
 		}
 
-		private int GetMaxDateTimeAlignment()
-		{
-			bool hasUniqueGameNames = GetHasUniqueGameNames();
-			if (hasUniqueGameNames)
-			{
-				return ComparisonRecords.Max(record => record.WrappedRecordInfo.DateTime.Length);
-			}
-			else
-			{
-				var maxGameNameLength = ComparisonRecords.Max(record => record.WrappedRecordInfo.Game.Length);
-				var maxDateTimeLength = ComparisonRecords.Max(record => record.WrappedRecordInfo.DateTime.Length);
-
-				return Math.Max(maxGameNameLength, maxDateTimeLength);
-			}
-		}
-
-		private int GetMaxCommentAlignment()
-		{
-			bool hasUniqueGameNames = GetHasUniqueGameNames();
-			if (hasUniqueGameNames)
-			{
-				return ComparisonRecords.Max(record
-				=> record.WrappedRecordInfo.FileRecordInfo.Comment.SplitWordWise(PART_LENGTH).Max(part => part.Length));
-			}
-			else
-			{
-				var maxGameNameLength = ComparisonRecords.Max(record => record.WrappedRecordInfo.Game.Length);
-				var maxContextLength = ComparisonRecords.Max(record
-					=> record.WrappedRecordInfo.FileRecordInfo.Comment.SplitWordWise(PART_LENGTH).Max(part => part.Length));
-
-				return Math.Max(maxGameNameLength, maxContextLength);
-			}
-		}
-
-		private int GetMaxGpuAlignment()
-		{
-			bool hasUniqueGameNames = GetHasUniqueGameNames();
-			if (hasUniqueGameNames)
-			{
-				return ComparisonRecords.Max(record
-				=> record.WrappedRecordInfo.FileRecordInfo.GraphicCardName.SplitWordWise(PART_LENGTH).Max(part => part.Length));
-			}
-			else
-			{
-				var maxGameNameLength = ComparisonRecords.Max(record => record.WrappedRecordInfo.Game.Length);
-				var maxGpuLength = ComparisonRecords.Max(record
-					=> record.WrappedRecordInfo.FileRecordInfo.GraphicCardName.SplitWordWise(PART_LENGTH).Max(part => part.Length));
-
-				return Math.Max(maxGameNameLength, maxGpuLength);
-			}
-		}
-
-		private int GetMaxSystemRamAlignment()
-		{
-			bool hasUniqueGameNames = GetHasUniqueGameNames();
-			if (hasUniqueGameNames)
-			{
-				return ComparisonRecords.Max(record
-				=> record.WrappedRecordInfo.FileRecordInfo.SystemRamInfo.SplitWordWise(PART_LENGTH).Max(part => part.Length));
-			}
-			else
-			{
-				var maxGameNameLength = ComparisonRecords.Max(record => record.WrappedRecordInfo.Game.Length);
-				var maxSystemRamLength = ComparisonRecords.Max(record
-					=> record.WrappedRecordInfo.FileRecordInfo.SystemRamInfo.SplitWordWise(PART_LENGTH).Max(part => part.Length));
-
-				return Math.Max(maxGameNameLength, maxSystemRamLength);
-			}
-		}
-
-		private int GetMaxCpuAlignment()
-		{
-			bool hasUniqueGameNames = GetHasUniqueGameNames();
-			if (hasUniqueGameNames)
-			{
-				return ComparisonRecords.Max(record
-				=> record.WrappedRecordInfo.FileRecordInfo.ProcessorName.SplitWordWise(PART_LENGTH).Max(part => part.Length));
-			}
-			else
-			{
-				var maxGameNameLength = ComparisonRecords.Max(record => record.WrappedRecordInfo.Game.Length);
-				var maxCpuLength = ComparisonRecords.Max(record
-					=> record.WrappedRecordInfo.FileRecordInfo.ProcessorName.SplitWordWise(PART_LENGTH).Max(part => part.Length));
-
-				return Math.Max(maxGameNameLength, maxCpuLength);
-			}
-		}
-
 		private string[] GetLabelForContext(ComparisonRecordInfoWrapper record, EComparisonContext context)
 		{
 			switch(context)
 			{
 				case EComparisonContext.CPU:
-					return GetLabelLines(record.WrappedRecordInfo.FileRecordInfo.ProcessorName, GetMaxCpuAlignment());
+					return GetLabelLines(record.WrappedRecordInfo.FileRecordInfo.ProcessorName);
 				case EComparisonContext.GPU:
-					return GetLabelLines(record.WrappedRecordInfo.FileRecordInfo.GraphicCardName, GetMaxGpuAlignment());
+					return GetLabelLines(record.WrappedRecordInfo.FileRecordInfo.GraphicCardName);
 				case EComparisonContext.SystemRam:
-					return GetLabelLines(record.WrappedRecordInfo.FileRecordInfo.SystemRamInfo, GetMaxSystemRamAlignment());
+					return GetLabelLines(record.WrappedRecordInfo.FileRecordInfo.SystemRamInfo);
 				case EComparisonContext.DateTime:
-					return GetLabelLines($"{record.WrappedRecordInfo.FileRecordInfo.CreationDate} { record.WrappedRecordInfo.FileRecordInfo.CreationTime}", GetMaxDateTimeAlignment());
+					return GetLabelLines($"{record.WrappedRecordInfo.FileRecordInfo.CreationDate} { record.WrappedRecordInfo.FileRecordInfo.CreationTime}");
 				case EComparisonContext.Custom:
-					return GetLabelLines(record.WrappedRecordInfo.FileRecordInfo.Comment, GetMaxCommentAlignment());
+					return GetLabelLines(record.WrappedRecordInfo.FileRecordInfo.Comment);
 				default:
 					return Array.Empty<string>();
 			}
 		}
 
-		private string[] GetLabelLines(string rawLabel, int maxAlignment)
+		private string[] GetLabelLines(string rawLabel)
 		{
 			if(string.IsNullOrWhiteSpace(rawLabel))
 			{
