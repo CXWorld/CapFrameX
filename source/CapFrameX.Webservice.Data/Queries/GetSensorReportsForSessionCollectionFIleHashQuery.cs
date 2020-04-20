531732e3360ce69b14ae7b7a007c753932a262af
Alexander Winnen <alwinnen@gmail.com>
2020-04-20 12:44:23 +0200

    add endpoint for sensorReport on website

---
+++ source/CapFrameX.Webservice.Data/Queries/GetSensorReportsForSessionCollectionFIleHashQuery.cs
@@ -0,0 +1,13 @@
+﻿using CapFrameX.Sensor.Reporting.Contracts;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CapFrameX.Webservice.Data.Queries
+{
+	public class GetSensorReportsForSessionCollectionFileHashQuery: IRequest<IEnumerable<ISensorReportItem>>
+	{
+		public Guid FileId { get; set; }
+	}
+}
