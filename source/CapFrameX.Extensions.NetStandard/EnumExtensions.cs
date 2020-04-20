45fdd8b5a65fd70bcf1b83475f4e72d19b6a3157
Alexander Winnen <alwinnen@gmail.com>
2020-04-20 11:32:00 +0200

    Extract Sensor Reporting into own .NetStandard Project

---
+++ source/CapFrameX.Extensions.NetStandard/EnumExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+namespace CapFrameX.Extensions.NetStandard
+{
+    public static class EnumExtensions
+    {
+        public static TAttribute GetAttribute<TAttribute>(this Enum value)
+            where TAttribute : Attribute
+        {
+            var type = value.GetType();
+            var name = Enum.GetName(type, value);
+            var attributes = type.GetField(name).GetCustomAttributes(false);
+            return attributes.OfType<TAttribute>().SingleOrDefault();
+        }
+    }
+}
