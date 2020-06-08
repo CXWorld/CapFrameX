2981d42d3632075f319b5988e4fcb94c3a333ba0
DevTechProfile <devtechprofile@hotmail.com>
2020-06-08 10:44:35 +0200

    refactored overly items group control

--- source/CapFrameX.ViewModel/SubModels/OverlayGroupControl.cs
+++ source/CapFrameX.ViewModel/SubModels/OverlayGroupControl.cs
@@ -1,8 +1,5 @@
-﻿using Prism.Commands;
-using Prism.Mvvm;
-using System;
+﻿using Prism.Mvvm;
 using System.Linq;
-using System.Windows.Input;
 
 namespace CapFrameX.ViewModel.SubModels
 {
@@ -13,223 +10,194 @@ namespace CapFrameX.ViewModel.SubModels
         private bool _overlayGroupCaptureItems;
         private bool _overlayGroupSystemInfo;
         private bool _overlayGroupOnlineMetrics;
+        private bool _overlayGroupGpuBasics;
         private bool _overlayGroupCPULoads;
         private bool _overlayGroupCPUClocks;
         private bool _overlayGroupCPUTemps;
-        private bool _overlayGroupCPUPackage;
         private bool _overlayGroupRAMItems;
-
-        private bool _useGroupCaptureItems;
-        private bool _useGroupSystemInfo;
-        private bool _useGroupOnlineMetrics;
-        private bool _useGroupCPULoads;
-        private bool _useGroupCPUClocks;
-        private bool _useGroupCPUTemps;
-        private bool _useGroupCPUPackage;
-        private bool _useGroupRAMItems;
 
         public bool OverlayGroupCaptureItems
         {
             get { return _overlayGroupCaptureItems; }
-            set { _overlayGroupCaptureItems = value; RaisePropertyChanged(); }
+            set
+            {
+                _overlayGroupCaptureItems = value;
+                RaisePropertyChanged();
+                ManageCXEntries();
+            }
         }
 
         public bool OverlayGroupSystemInfo
         {
             get { return _overlayGroupSystemInfo; }
-            set { _overlayGroupSystemInfo = value; RaisePropertyChanged(); }
+            set
+            {
+                _overlayGroupSystemInfo = value;
+                RaisePropertyChanged();
+                ManageSystemInfoEntries();
+            }
         }
 
         public bool OverlayGroupOnlineMetrics
         {
             get { return _overlayGroupOnlineMetrics; }
-            set { _overlayGroupOnlineMetrics = value; RaisePropertyChanged(); }
+            set
+            {
+                _overlayGroupOnlineMetrics = value;
+                RaisePropertyChanged();
+                ManageOnlineMetricEntries();
+            }
+        }
+
+        public bool OverlayGroupGpuBasics
+        {
+            get { return _overlayGroupGpuBasics; }
+            set
+            {
+                _overlayGroupGpuBasics = value;
+                RaisePropertyChanged();
+                ManageGpuBasicEntries();
+            }
         }
 
         public bool OverlayGroupCPULoads
         {
             get { return _overlayGroupCPULoads; }
-            set { _overlayGroupCPULoads = value; RaisePropertyChanged(); }
+            set
+            {
+                _overlayGroupCPULoads = value;
+                RaisePropertyChanged();
+                ManageCPULoadEntries();
+            }
         }
 
         public bool OverlayGroupCPUClocks
         {
             get { return _overlayGroupCPUClocks; }
-            set { _overlayGroupCPUClocks = value; RaisePropertyChanged(); }
+            set
+            {
+                _overlayGroupCPUClocks = value;
+                RaisePropertyChanged();
+                ManageCPUClockEntries();
+            }
         }
 
         public bool OverlayGroupCPUTemps
         {
             get { return _overlayGroupCPUTemps; }
-            set { _overlayGroupCPUTemps = value; RaisePropertyChanged(); }
-        }
-
-        public bool OverlayGroupCPUPackage
-        {
-            get { return _overlayGroupCPUPackage; }
-            set { _overlayGroupCPUPackage = value; RaisePropertyChanged(); }
+            set
+            {
+                _overlayGroupCPUTemps = value;
+                RaisePropertyChanged();
+                ManageCPUTemperatureEntries();
+            }
         }
 
         public bool OverlayGroupRAMItems
         {
             get { return _overlayGroupRAMItems; }
-            set { _overlayGroupRAMItems = value; RaisePropertyChanged(); }
+            set
+            {
+                _overlayGroupRAMItems = value;
+                RaisePropertyChanged();
+                ManageRAMEntries();
+            }
         }
-
-        public bool UseGroupCaptureItems
-        {
-            get { return _useGroupCaptureItems; }
-            set { _useGroupCaptureItems = value; RaisePropertyChanged(); }
-        }
-
-        public bool UseGroupSystemInfo
-        {
-            get { return _useGroupSystemInfo; }
-            set { _useGroupSystemInfo = value; RaisePropertyChanged(); }
-        }
-
-        public bool UseGroupOnlineMetrics
-        {
-            get { return _useGroupOnlineMetrics; }
-            set { _useGroupOnlineMetrics = value; RaisePropertyChanged(); }
-        }
-
-        public bool UseGroupCPULoads
-        {
-            get { return _useGroupCPULoads; }
-            set { _useGroupCPULoads = value; RaisePropertyChanged(); }
-        }
-
-        public bool UseGroupCPUClocks
-        {
-            get { return _useGroupCPUClocks; }
-            set { _useGroupCPUClocks = value; RaisePropertyChanged(); }
-        }
-
-        public bool UseGroupCPUTemps
-        {
-            get { return _useGroupCPUTemps; }
-            set { _useGroupCPUTemps = value; RaisePropertyChanged(); }
-        }
-
-        public bool UseGroupCPUPackage
-        {
-            get { return _useGroupCPUPackage; }
-            set { _useGroupCPUPackage = value; RaisePropertyChanged(); }
-        }
-
-        public bool UseGroupRAMItems
-        {
-            get { return _useGroupRAMItems; }
-            set { _useGroupRAMItems = value; RaisePropertyChanged(); }
-        }
-
-        public ICommand AcceptOSDGroupSettingsCommand { get; }
 
         public OverlayGroupControl(OverlayViewModel overlayViewModel)
         {
             _overlayViewModel = overlayViewModel;
-
-            AcceptOSDGroupSettingsCommand = new DelegateCommand( () => OnGroupSettingsChanged());
         }
 
-        private void OnGroupSettingsChanged()
+        private void ManageCXEntries()
         {
-            // manage CX entries
-            if (UseGroupCaptureItems)
+            foreach (var entry in _overlayViewModel.OverlayEntries
+                   .Where(item => item.Identifier == "CaptureServiceStatus"
+                       || item.Identifier == "CaptureTimer"
+                       || item.Identifier == "RunHistory"))
             {
-                foreach (var entry in _overlayViewModel.OverlayEntries
-                    .Where(item => item.Identifier == "CaptureServiceStatus"
-                        || item.Identifier == "CaptureTimer"
-                        || item.Identifier == "RunHistory"))
-                {
-                    if(entry.ShowOnOverlayIsEnabled)
-                        entry.ShowOnOverlay = OverlayGroupCaptureItems;
-                }
+                if (entry.ShowOnOverlayIsEnabled)
+                    entry.ShowOnOverlay = OverlayGroupCaptureItems;
             }
+        }
 
-            // manage system info entries
-            if (UseGroupSystemInfo)
+        private void ManageSystemInfoEntries()
+        {
+            foreach (var entry in _overlayViewModel.OverlayEntries
+                   .Where(item => item.Identifier == "CustomCPU"
+                       || item.Identifier == "CustomGPU"
+                       || item.Identifier == "Mainboard"
+                       || item.Identifier == "CustomRAM"
+                       || item.Identifier == "OS"
+                       || item.Identifier == "GPUDriver"))
             {
-                foreach (var entry in _overlayViewModel.OverlayEntries
-                    .Where(item => item.Identifier == "CustomCPU"
-                        || item.Identifier == "CustomGPU"
-                        || item.Identifier == "Mainboard"
-                        || item.Identifier == "CustomRAM"
-                        || item.Identifier == "OS"
-                        || item.Identifier == "GPUDriver"))
-                {
-                    if (entry.ShowOnOverlayIsEnabled)
-                        entry.ShowOnOverlay = OverlayGroupSystemInfo;
-                }
+                if (entry.ShowOnOverlayIsEnabled)
+                    entry.ShowOnOverlay = OverlayGroupSystemInfo;
             }
+        }
 
-            // manage online metrics entries
-            if (UseGroupOnlineMetrics)
+        private void ManageOnlineMetricEntries()
+        {
+            foreach (var entry in _overlayViewModel.OverlayEntries
+                   .Where(item => item.Identifier == "OnlineAverage"
+                       || item.Identifier == "OnlineP1"
+                       || item.Identifier == "OnlineP0dot2"))
             {
-                foreach (var entry in _overlayViewModel.OverlayEntries
-                    .Where(item => item.Identifier == "OnlineAverage"
-                        || item.Identifier == "OnlineP1"
-                        || item.Identifier == "OnlineP0dot2"))
-                {
-                    if (entry.ShowOnOverlayIsEnabled)
-                        entry.ShowOnOverlay = OverlayGroupOnlineMetrics;
-                }
+                if (entry.ShowOnOverlayIsEnabled)
+                    entry.ShowOnOverlay = OverlayGroupOnlineMetrics;
             }
+        }
 
-            // manage CPU load entries
-            if (UseGroupCPULoads)
+        private void ManageGpuBasicEntries()
+        {
+            foreach (var entry in _overlayViewModel.OverlayEntries
+                 .Where(item => item.OverlayEntryType == Contracts.Overlay.EOverlayEntryType.GPU))
             {
-                foreach (var entry in _overlayViewModel.OverlayEntries
-                    .Where(item => item.OverlayEntryType == Contracts.Overlay.EOverlayEntryType.CPU))
-                {
-                    if (entry.Identifier.Contains("load") && entry.ShowOnOverlayIsEnabled)
-                        entry.ShowOnOverlay = OverlayGroupCPULoads;
-                }
+                if (entry.Description.Contains("GPU Core") && entry.ShowOnOverlayIsEnabled)
+                    entry.ShowOnOverlay = OverlayGroupGpuBasics;
             }
+        }
 
-            // manage CPU clocks entries
-            if (UseGroupCPUClocks)
+        private void ManageCPULoadEntries()
+        {
+            foreach (var entry in _overlayViewModel.OverlayEntries
+                   .Where(item => item.OverlayEntryType == Contracts.Overlay.EOverlayEntryType.CPU))
             {
-                foreach (var entry in _overlayViewModel.OverlayEntries
-                    .Where(item => item.OverlayEntryType == Contracts.Overlay.EOverlayEntryType.CPU))
-                {
-                    if (entry.Identifier.Contains("clock") && entry.ShowOnOverlayIsEnabled)
-                        entry.ShowOnOverlay = OverlayGroupCPUClocks;
-                }
+                if (entry.Identifier.Contains("load") && entry.Description.Contains("CPU Core") 
+                    && entry.ShowOnOverlayIsEnabled)
+                    entry.ShowOnOverlay = OverlayGroupCPULoads;
             }
+        }
 
-            // manage CPU temps entries
-            if (UseGroupCPUTemps)
+        private void ManageCPUClockEntries()
+        {
+            foreach (var entry in _overlayViewModel.OverlayEntries
+                   .Where(item => item.OverlayEntryType == Contracts.Overlay.EOverlayEntryType.CPU))
             {
-                foreach (var entry in _overlayViewModel.OverlayEntries
-                    .Where(item => item.OverlayEntryType == Contracts.Overlay.EOverlayEntryType.CPU))
-                {
-                    if (entry.Identifier.Contains("temperature") && entry.ShowOnOverlayIsEnabled)
-                        entry.ShowOnOverlay = OverlayGroupCPUTemps;
-                }
+                if (entry.Identifier.Contains("clock") && entry.Description.Contains("CPU Core") 
+                    && entry.ShowOnOverlayIsEnabled)
+                    entry.ShowOnOverlay = OverlayGroupCPUClocks;
             }
+        }
 
-            // manage CPU package entries
-            if (UseGroupCPUPackage)
+        private void ManageCPUTemperatureEntries()
+        {
+            foreach (var entry in _overlayViewModel.OverlayEntries
+                   .Where(item => item.OverlayEntryType == Contracts.Overlay.EOverlayEntryType.CPU))
             {
-                foreach (var entry in _overlayViewModel.OverlayEntries
-                    .Where(item => item.OverlayEntryType == Contracts.Overlay.EOverlayEntryType.CPU))
-                {
-                    if (entry.Identifier.Contains("power") && entry.ShowOnOverlayIsEnabled)
-                        entry.ShowOnOverlay = OverlayGroupCPUPackage;
-                }
+                if (entry.Identifier.Contains("temperature") && entry.Description.Contains("CPU Core") 
+                    && entry.ShowOnOverlayIsEnabled)
+                    entry.ShowOnOverlay = OverlayGroupCPUTemps;
             }
+        }
 
-            // manage RAM entries
-            if (UseGroupRAMItems)
+        private void ManageRAMEntries()
+        {
+            foreach (var entry in _overlayViewModel.OverlayEntries
+                   .Where(item => item.OverlayEntryType == Contracts.Overlay.EOverlayEntryType.RAM))
             {
-                foreach (var entry in _overlayViewModel.OverlayEntries
-                    .Where(item => item.OverlayEntryType == Contracts.Overlay.EOverlayEntryType.RAM))
-                {
-                    if (entry.ShowOnOverlayIsEnabled)
-                        entry.ShowOnOverlay = OverlayGroupRAMItems;
-                }
+                if (entry.ShowOnOverlayIsEnabled)
+                    entry.ShowOnOverlay = OverlayGroupRAMItems;
             }
         }
     }
