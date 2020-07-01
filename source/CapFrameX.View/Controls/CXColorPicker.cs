b01ecd48514470297faa637007eadf345ff57752
DevTechProfile <devtechprofile@hotmail.com>
2020-07-01 12:16:43 +0200

    CXColorPicker

---
+++ source/CapFrameX.View/Controls/CXColorPicker.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows.Media;
+using Xceed.Wpf.Toolkit;
+
+namespace CapFrameX.View.Controls
+{
+    public class CXColorPicker : ColorPicker
+    {
+        public CXColorPicker(): base()
+        {
+            StandardColors = new System.Collections.ObjectModel.ObservableCollection<ColorItem>
+            {
+                new ColorItem(Colors.Blue, "CX blue")
+            };
+        }
+    }
+}
