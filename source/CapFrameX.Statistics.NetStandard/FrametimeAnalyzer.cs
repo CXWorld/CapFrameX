55ab46205156d51e468167c9894f2d97def6e02a
Taxxor <taxxor90@gmail.com>
2020-08-17 17:23:34 +0200

    l-shape copy order

--- source/CapFrameX.Statistics.NetStandard/FrametimeAnalyzer.cs
+++ source/CapFrameX.Statistics.NetStandard/FrametimeAnalyzer.cs
@@ -12,7 +12,7 @@ namespace CapFrameX.Statistics.NetStandard
 					= new[] { 90.0, 91.0, 92.0, 93.0, 94.0, 95.0, 96.0, 97.0, 98.0, 99.0, 99.5, 99.8, 99.9, 99.95 };
 
 		private static readonly double[] _lShapeFPSQuantiles
-			= new[] { 10.0, 9.0, 8.0, 7.0, 6.0, 5.0, 4.0, 3.0, 2.0, 1.0, 0.5, 0.2, 0.1, 0.05 };
+			= new[] { 0.05, 0.1, 0.2, 0.5, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0 };
 
 		public double[] GetLShapeQuantiles(ELShapeMetrics LShapeMetric)
 		{
