4d5aa4df6b16c1e1e4330f52e82f18cbfea7932f
DevTechProfile <devtechprofile@hotmail.com>
2020-09-25 18:57:21 +0200

    Simplification function

--- source/CapFrameX.Extensions/ObservableExtensions.cs
+++ source/CapFrameX.Extensions/ObservableExtensions.cs
@@ -4,15 +4,14 @@ using System.Reactive.Linq;
 
 namespace CapFrameX.Extensions
 {
-	public static class ObservableExtensions
-	{
-		public static IObservable<long> CountDown(double doubleseconds)
-		{
-			var seconds = Convert.ToInt32(doubleseconds);
-			return Observable
-				.Timer(DateTimeOffset.UtcNow, TimeSpan.FromSeconds(1))
-				.Select(i => seconds - i)
-				.Take(seconds + 1);
-		}
-	}
+    public static class ObservableExtensions
+    {
+        public static IObservable<long> CountDown(double seconds)
+        {
+            return Observable
+                .Timer(DateTimeOffset.UtcNow, TimeSpan.FromSeconds(1))
+                .Select(i => (int)seconds - i)
+                .Take((int)seconds + 1);
+        }
+    }
 }
