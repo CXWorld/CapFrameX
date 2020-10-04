843e385d7fa1d58c19cc75b1eec9196863e50da3
Alexander Winnen <alwinnen@gmail.com>
2020-10-04 17:05:21 +0200

    remove params

--- source/CapFrameX.ApiInterface/JsonPayload/StartCapturePayload.cs
+++ source/CapFrameX.ApiInterface/JsonPayload/StartCapturePayload.cs
@@ -11,9 +11,6 @@ namespace CapFrameX.Remote.JsonPayload
     {
         public double CaptureTime { get; set; }
         public string ProcessName { get; set; }
-        public bool UseRunHistory { get; set; }
-        public bool UseAggregation { get; set; }
-        public bool SaveAggregationOnly { get; set; }
         public string CaptureFileMode { get; set; } = Enum.GetName(typeof(ECaptureFileMode), ECaptureFileMode.Json);
         public string RecordDirectory { get; set; }
     }
