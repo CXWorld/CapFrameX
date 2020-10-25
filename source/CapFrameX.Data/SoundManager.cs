5bcd695980edc008767ed3b0bcd11d912eb38ceb
DevTechProfile <devtechprofile@hotmail.com>
2020-10-25 17:48:48 +0100

    multi player approach

--- source/CapFrameX.Data/SoundManager.cs
+++ source/CapFrameX.Data/SoundManager.cs
@@ -1,7 +1,10 @@
 ﻿using CapFrameX.Contracts.Configuration;
+using CapFrameX.Extensions;
 using CapFrameX.Extensions.NetStandard;
 using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Media;
 
@@ -9,7 +12,8 @@ namespace CapFrameX.Data
 {
     public class SoundManager
     {
-        private readonly MediaPlayer _soundPlayer = new MediaPlayer();
+        private readonly MediaPlayer[] _soundPlayers = new MediaPlayer[6];
+        private readonly Dictionary<string, int> _playerIndexDict = new Dictionary<string, int>(6);
         private readonly IAppConfiguration _configuration;
 
         public SoundMode SoundMode
@@ -65,6 +69,43 @@ namespace CapFrameX.Data
         public SoundManager(IAppConfiguration configuration)
         {
             _configuration = configuration;
+            string soundPath;
+
+            // capture started (voice)
+            _soundPlayers[0] = new MediaPlayer();
+            soundPath = Path.Combine("Sounds", SoundMode.Voice.ConvertToString(), $"{Sound.CaptureStarted.ConvertToString()}.mp3");
+            _playerIndexDict.Add(soundPath, 0);
+            _soundPlayers[0].Open(new Uri(soundPath, UriKind.Relative));
+
+            // capture started (simple)
+            _soundPlayers[1] = new MediaPlayer();
+            soundPath = Path.Combine("Sounds", SoundMode.Simple.ConvertToString(), $"{Sound.CaptureStarted.ConvertToString()}.mp3");
+            _playerIndexDict.Add(soundPath, 1);
+            _soundPlayers[1].Open(new Uri(soundPath, UriKind.Relative));
+
+            // capture stopped (voice)
+            _soundPlayers[2] = new MediaPlayer();
+            soundPath = Path.Combine("Sounds", SoundMode.Voice.ConvertToString(), $"{Sound.CaptureStopped.ConvertToString()}.mp3");
+            _playerIndexDict.Add(soundPath, 2);
+            _soundPlayers[2].Open(new Uri(soundPath, UriKind.Relative));
+
+            // capture stopped (simple)
+            _soundPlayers[3] = new MediaPlayer();
+            soundPath = Path.Combine("Sounds", SoundMode.Simple.ConvertToString(), $"{Sound.CaptureStopped.ConvertToString()}.mp3");
+            _playerIndexDict.Add(soundPath, 3);
+            _soundPlayers[3].Open(new Uri(soundPath, UriKind.Relative));
+
+            // more than one process (voice)
+            _soundPlayers[4] = new MediaPlayer();
+            soundPath = Path.Combine("Sounds", SoundMode.Voice.ConvertToString(), $"{Sound.MoreThanOneProcess.ConvertToString()}.mp3");
+            _playerIndexDict.Add(soundPath, 4);
+            _soundPlayers[4].Open(new Uri(soundPath, UriKind.Relative));
+
+            // no process detected (voice)
+            _soundPlayers[5] = new MediaPlayer();
+            soundPath = Path.Combine("Sounds", SoundMode.Voice.ConvertToString(), $"{Sound.NoProcess.ConvertToString()}.mp3");
+            _playerIndexDict.Add(soundPath, 5);
+            _soundPlayers[5].Open(new Uri(soundPath, UriKind.Relative));
         }
 
         public void PlaySound(Sound sound)
@@ -79,15 +120,11 @@ namespace CapFrameX.Data
             Application.Current.Dispatcher.Invoke(() =>
             {
                 var path = Path.Combine("Sounds", currentSoundMode.ConvertToString(), $"{sound.ConvertToString()}.mp3");
-                PlayFile(new Uri(path, UriKind.Relative), currentVolume);
+                var player = _soundPlayers[_playerIndexDict[path]];
+                player.Volume = currentVolume;
+                player.Play();
+                player.Open(new Uri(path, UriKind.Relative));
             });
-        }
-
-        private void PlayFile(Uri uri, double volume)
-        {
-            _soundPlayer.Open(uri);
-            _soundPlayer.Volume = volume;
-            _soundPlayer.Play();
         }
 
         public void SetSoundMode(string value)
