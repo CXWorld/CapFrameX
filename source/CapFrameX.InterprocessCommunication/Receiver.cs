e724c29296c96d4aee06219ca1ee99d578b9b018
Alexander Winnen <alwinnen@gmail.com>
2020-11-13 20:59:42 +0100

    skeleton InterprocessCommunication

---
+++ source/CapFrameX.InterprocessCommunication/Receiver.cs
@@ -0,0 +1,32 @@
+﻿using CapFrameX.InterprocessCommunication.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapFrameX.InterprocessCommunication
+{
+    public class Receiver: IReceiver
+    {
+        public void RegisterOnOSDOff(Action<object> callBack)
+        {
+            RegisterTopic("OSDOff", callBack);
+        }
+
+        public void RegisterOnOSDOn(Action<object> callBack)
+        {
+            RegisterTopic("OSDOn", callBack);
+        }
+
+        public void RegisterOnOSDToggle(Action<object> callBack)
+        {
+            RegisterTopic("OSDToggle", callBack);
+        }
+
+        private void RegisterTopic(string topic, Action<object> onEventReceived)
+        {
+
+        }
+    }
+}
---
+++ source/CapFrameX.InterprocessCommunication/Sender.cs
@@ -0,0 +1,27 @@
+﻿using CapFrameX.InterprocessCommunication.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapFrameX.InterprocessCommunication
+{
+    public class Sender : ISender
+    {
+        public void OSDOff()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OSDOn()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OSDToggle()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
