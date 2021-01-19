2e1cb28e4e84c5d95c2b8c39c72c117a10964643
Taxxor90 <taxxor90@gmail.com>
2021-01-19 21:02:24 +0100

    sensor statistics data grid

--- source/CapFrameX.ViewModel/SensorViewModel.cs
+++ source/CapFrameX.ViewModel/SensorViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using CapFrameX.Contracts.Configuration;
+using CapFrameX.Contracts.Data;
 using CapFrameX.Contracts.Sensor;
 using CapFrameX.Data;
 using CapFrameX.Data.Session.Contracts;
@@ -15,7 +16,9 @@ using Prism.Regions;
 using System;
 using System.Collections.ObjectModel;
 using System.Diagnostics;
+using System.Globalization;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Input;
@@ -28,12 +31,16 @@ namespace CapFrameX.ViewModel
         private readonly IEventAggregator _eventAggregator;
         private readonly IAppConfiguration _appConfiguration;
         private readonly ISensorEntryProvider _sensorEntryProvider;
+        private readonly IRecordDataServer _localRecordDataServer;
         private readonly ILogger<SensorViewModel> _logger;
         private readonly CaptureManager _captureManager;
 
+        private ISession _session;
         private int _selectedSensorEntryIndex;
         private SensorEntryWrapper _selectedSensorEntry;
         private bool _saveButtonIsEnable;
+
+        public IFileRecordInfo RecordInfo { get; private set; }
 
         public bool UseSensorLogging
         {
@@ -104,6 +111,10 @@ namespace CapFrameX.ViewModel
 
         public ICommand ResetToDefaultCommand { get; }
 
+        public ICommand CopySensorInfoCommand { get; }
+
+        public ICommand CopyRawSensorInfoCommand { get; }
+
         public SensorGroupControl SensorSubModelGroupControl { get; }
 
         public SensorViewModel(IAppConfiguration appConfiguration,
@@ -123,7 +134,7 @@ namespace CapFrameX.ViewModel
             _logger = logger;
             _captureManager = captureManager;
 
-
+            _localRecordDataServer = new LocalRecordDataServer();
             // define submodels
             SensorSubModelGroupControl = new SensorGroupControl(this);
 
@@ -134,11 +145,15 @@ namespace CapFrameX.ViewModel
                     SaveButtonIsEnable = false;
                 });
 
+            CopySensorInfoCommand = new DelegateCommand(OnCopySensorInfo);
+            CopyRawSensorInfoCommand = new DelegateCommand(OnCopyRawSensorInfo);
             ResetToDefaultCommand = new DelegateCommand(OnResetToDefault);
             _sensorEntryProvider.ConfigChanged = () => SaveButtonIsEnable = true;
 
             Task.Run(async () => await SetWrappedSensorEntries());
             SubscribeToUpdateSession();
+
+
 
             stopwatch.Stop();
             _logger.LogInformation(this.GetType().Name + " {initializationTime}s initialization time", Math.Round(stopwatch.ElapsedMilliseconds * 1E-03, 1));
@@ -167,6 +182,8 @@ namespace CapFrameX.ViewModel
             _eventAggregator.GetEvent<PubSubEvent<ViewMessages.UpdateSession>>()
                             .Subscribe(msg =>
                             {
+                                _session = msg.CurrentSession;
+                                RecordInfo = msg.RecordInfo;
                                 UpdateSensorSessionReport(msg.CurrentSession);
                             });
         }
@@ -174,11 +191,60 @@ namespace CapFrameX.ViewModel
         private void UpdateSensorSessionReport(ISession session)
         {
             SensorReportItems.Clear();
-            var items = SensorReport.GetReportFromSessionSensorData(session.Runs.Select(run => run.SensorData2).Cast<ISessionSensorData>());
+
+            if (RecordInfo == null)
+                return;
+           
+            var items = SensorReport.GetFullReportFromSessionSensorData(session.Runs.Select(run => run.SensorData2));
             foreach (var item in items)
             {
                 SensorReportItems.Add(item);
             };
+        }
+
+        private void OnCopySensorInfo()
+        {
+            if (RecordInfo == null)
+                return;
+
+            var sensorInfos = SensorReport.GetFullReportFromSessionSensorData(_session.Runs.Select(run => run.SensorData2));
+
+            StringBuilder builder = new StringBuilder();
+
+            foreach (var sensorInfo in sensorInfos)
+            {
+                builder.Append(sensorInfo.Name + "\t" + sensorInfo.MinValue + "\t" +
+                sensorInfo.AverageValue + "\t" + sensorInfo.MaxValue + Environment.NewLine);
+            }
+
+            Clipboard.SetDataObject(builder.ToString(), false);
+        }
+
+        private void OnCopyRawSensorInfo()
+        {
+            if (RecordInfo == null)
+                return;
+
+            var data = SensorReport.GetSensorReportEntries(_session.Runs.Select(s => s.SensorData2))
+                .Where(x => x.Name != "BetweenMeasureTime");
+
+
+            StringBuilder builder = new StringBuilder();
+
+            // Header
+            builder.AppendLine(string.Join("\t", data.Select(x => x.DisplayName)));
+
+
+
+            for (int i = 0; i < data.First().Values.Count(); i++)
+            {
+                builder.AppendLine(string.Join("\t", data.Select(x => {
+                    SensorReport.roundingDictionary.TryGetValue(x.Type, out var roundingDigit);
+                    return Math.Round(x.Values[i], roundingDigit);
+                })));
+            }
+
+            Clipboard.SetDataObject(builder.ToString(), false);
         }
 
         public bool IsNavigationTarget(NavigationContext navigationContext)
