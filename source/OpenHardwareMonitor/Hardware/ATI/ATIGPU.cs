741c59a8279fa3313af80513b9f8c522a7eaf1a6
devtechprofile <devtechprofile@hotmail.com>
2021-01-22 16:28:18 +0100

    better performance counter handling for AMD GPUs

--- source/OpenHardwareMonitor/Hardware/ATI/ATIGPU.cs
+++ source/OpenHardwareMonitor/Hardware/ATI/ATIGPU.cs
@@ -124,15 +124,32 @@ namespace OpenHardwareMonitor.Hardware.ATI
                     var category = new PerformanceCounterCategory("GPU Adapter Memory");
                     var instances = category.GetInstanceNames();
 
-                    var (Usage, Index) = instances
-                        .Select(instance => new PerformanceCounter("GPU Adapter Memory", "Dedicated Usage", instance))
-                        .Select((u, i) => (Usage: u.RawValue, Index: i)).Max();
+                    if (instances.Any())
+                    {
+                        long maxRawValue = 0;
+                        int maxIndex = 0;
+                        for (int i = 0; i < instances.Length; i++)
+                        {
+                            try
+                            {
+                                var currentPerfCounter = new PerformanceCounter("GPU Adapter Memory", "Dedicated Usage", instances[i]);
 
-                    dedicatedVramUsagePerformCounter = new PerformanceCounter("GPU Adapter Memory", "Dedicated Usage", instances[Index]);
-                    sharedVramUsagePerformCounter = new PerformanceCounter("GPU Adapter Memory", "Shared Usage", instances[Index]);
+                                if (currentPerfCounter.RawValue > maxRawValue)
+                                {
+                                    maxRawValue = currentPerfCounter.RawValue;
+                                    maxIndex = i;
+                                }
 
-                    this.memoryUsageDedicated = new Sensor("GPU Memory Dedicated", 0, SensorType.SmallData, this, settings);
-                    this.memoryUsageShared = new Sensor("GPU Memory Shared", 1, SensorType.SmallData, this, settings);
+                            }
+                            catch (Exception ex)
+                            {
+                                Log.Logger.Error(ex, $"Error while creating performance counter with instance {i}.");
+                            }
+                        }
+
+                        dedicatedVramUsagePerformCounter = new PerformanceCounter("GPU Adapter Memory", "Dedicated Usage", instances[maxIndex]);
+                        sharedVramUsagePerformCounter = new PerformanceCounter("GPU Adapter Memory", "Shared Usage", instances[maxIndex]);
+                    }
                 }
             }
             catch (Exception ex)
