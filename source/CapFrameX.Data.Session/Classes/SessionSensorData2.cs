5229c134cf6abb73d3df3f0cc9e3e488e0797709
Taxxor90 <taxxor90@gmail.com>
2021-01-23 23:12:25 +0100

    filtered sensor data on Analysis

--- source/CapFrameX.Data.Session/Classes/SessionSensorData2.cs
+++ source/CapFrameX.Data.Session/Classes/SessionSensorData2.cs
@@ -46,9 +46,9 @@ namespace CapFrameX.Data.Session.Classes
         [JsonIgnore]
         public int[] GpuTemp { get => Values.FirstOrDefault(c => c.Name.Contains("GPU Core") && c.Type == "Temperature")?.Values.Select(Convert.ToInt32).ToArray() ?? Array.Empty<int>(); set => throw new NotImplementedException(); }
         [JsonIgnore]
-        public double[] RamUsage { get => Values.FirstOrDefault(c => c.Name.Contains("Used Memory") && c.Type == "Data")?.Values.ToArray() ?? Array.Empty<double>(); set => throw new NotImplementedException(); }
+        public double[] RamUsage { get => Values.FirstOrDefault(c => c.Name.Contains("Used Memory") && !c.Name.Contains("Game") && c.Type == "Data")?.Values.ToArray() ?? Array.Empty<double>(); set => throw new NotImplementedException(); }
         [JsonIgnore]
-        public int[] VRamUsage { get => Values.FirstOrDefault(c => c.Name.Contains("Dedicated") && c.Type == "SmallData")?.Values.Select(Convert.ToInt32).ToArray() ?? Array.Empty<int>(); set => throw new NotImplementedException(); }
+        public int[] VRamUsage { get => Values.FirstOrDefault(c => c.Name.Contains("Dedicated") && !c.Name.Contains("Game") && c.Type == "SmallData")?.Values.Select(Convert.ToInt32).ToArray() ?? Array.Empty<int>(); set => throw new NotImplementedException(); }
         [JsonIgnore]
         public double[] BetweenMeasureTimes { get => BetweenMeasureTime.Values.ToArray(); set => throw new NotImplementedException(); }
 
