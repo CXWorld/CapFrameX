144d08ce6f9a8d33fa205003873e76ad3ed515a3
DevTechProfile <devtechprofile@hotmail.com>
2021-05-13 09:12:38 +0200

    Nuget source, using block

--- source/CapFrameX.SetupAPI.NetStandard/SetupAPIWrapper.cs
+++ source/CapFrameX.SetupAPI.NetStandard/SetupAPIWrapper.cs
@@ -1,10 +1,12 @@
 ﻿using System.Linq;
-
 using Mixaill.HwInfo.SetupApi;
 using Mixaill.HwInfo.SetupApi.Defines;
 
 namespace CapFrameX.SetupAPI.NetStandard
 {
+    /// <summary>
+    /// Nuget https://github.com/Mixaill/Mixaill.HwInfo
+    /// </summary>
     public class SetupAPIWrapper : ISetupAPI
     {
         public bool PciAbove4GDecodingStatus { get; } = false;
@@ -12,11 +14,11 @@ namespace CapFrameX.SetupAPI.NetStandard
 
         public SetupAPIWrapper()
         {
-            var displayDevices = new DeviceInfoSet(DeviceClassGuid.Display);
-            PciAbove4GDecodingStatus = displayDevices.Devices.Any(x => (x as DeviceInfoPci)?.Pci_Above4GDecoding == true);
-            PciLargeMemoryStatus = displayDevices.Devices.Any(x => (x as DeviceInfoPci)?.Pci_LargeMemory == true);
-
-            displayDevices.Dispose();
+            using (var displayDevices = new DeviceInfoSet(DeviceClassGuid.Display))
+            {
+                PciAbove4GDecodingStatus = displayDevices.Devices.Any(x => (x as DeviceInfoPci)?.Pci_Above4GDecoding == true);
+                PciLargeMemoryStatus = displayDevices.Devices.Any(x => (x as DeviceInfoPci)?.Pci_LargeMemory == true);
+            }
         }
     }
 }
