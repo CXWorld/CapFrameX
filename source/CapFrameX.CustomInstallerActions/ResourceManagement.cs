81134b8e12af919e63b4dcfd8f98976c16aaf15c
DevTechProfile <devtechprofile@hotmail.com>
2021-05-15 21:34:03 +0200

    ResourceManagement custom action

---
+++ source/CapFrameX.CustomInstallerActions/ResourceManagement.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Deployment.WindowsInstaller;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapFrameX.CustomInstallerActions
+{
+    public class ResourceManagement
+    {
+        [CustomAction]
+        public static ActionResult CleanupConfigResources(Session session)
+        {
+            session.Log("Begin RemoveAutoStartKey");
+
+            try
+            {
+               
+            }
+            catch { }
+
+            return ActionResult.Success;
+        }
+    }
+}
