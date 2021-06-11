9584ef6ddbe78cf6f6956f7511e94c22998ae6ee
devtechprofile <devtechprofile@hotmail.com>
2021-06-11 17:51:16 +0200

    code cleanup LogEntryManager

--- source/CapFrameX.Data/Logging/LogEntryManager.cs
+++ source/CapFrameX.Data/Logging/LogEntryManager.cs
@@ -24,31 +24,17 @@ namespace CapFrameX.Data.Logging
 
         public void AddLogEntry(string message, ELogMessageType messageType)
         {
-            var logEntry = new LogEntry()
+            Application.Current.Dispatcher.BeginInvoke((Action)(() =>
             {
-                MessageType = messageType,
-                MessageInfo = DateTime.Now.ToString("HH:mm:ss") + $" ( Type: {messageType.GetDescription()} )",
-                Message = message
-            };
-
-            _logEntryHistory.Add(logEntry);
-
-            Application.Current.Dispatcher.BeginInvoke(DispatcherPriority.Normal, (Action)(() =>
-            {
-                switch (messageType)
+                var logEntry = new LogEntry()
                 {
-                    case ELogMessageType.Error when ShowErrors:
-                        LogEntryOutput.Add(logEntry);
-                        break;
-                    case ELogMessageType.BasicInfo when ShowBasicInfo:
-                        LogEntryOutput.Add(logEntry);
-                        break;
-                    case ELogMessageType.AdvancedInfo when ShowAdvancedInfo:
-                        LogEntryOutput.Add(logEntry);
-                        break;
-                    default:
-                        break;
-                }              
+                    MessageType = messageType,
+                    MessageInfo = DateTime.Now.ToString("HH:mm:ss") + $" ( Type: {messageType.GetDescription()} )",
+                    Message = message
+                };
+
+                _logEntryHistory.Add(logEntry);
+                AddLogEntryByFilter(logEntry);
             }));
         }
 
@@ -57,25 +43,26 @@ namespace CapFrameX.Data.Logging
             Application.Current.Dispatcher.BeginInvoke(DispatcherPriority.Normal, (Action)(() =>
             {
                 LogEntryOutput = new ObservableCollection<ILogEntry>();
+                _logEntryHistory.ForEach(AddLogEntryByFilter);           
+            }));           
+        }
 
-                foreach (var entry in _logEntryHistory)
-                {
-                    switch (entry.MessageType)
-                    {
-                        case ELogMessageType.Error when ShowErrors:
-                            LogEntryOutput.Add(entry);
-                            break;
-                        case ELogMessageType.BasicInfo when ShowBasicInfo:
-                            LogEntryOutput.Add(entry);
-                            break;
-                        case ELogMessageType.AdvancedInfo when ShowAdvancedInfo:
-                            LogEntryOutput.Add(entry);
-                            break;
-                        default:
-                            break;
-                    }
-                }               
-            }));
+        private void AddLogEntryByFilter(ILogEntry logEntry)
+        {
+            switch (logEntry.MessageType)
+            {
+                case ELogMessageType.Error when ShowErrors:
+                    LogEntryOutput.Add(logEntry);
+                    break;
+                case ELogMessageType.BasicInfo when ShowBasicInfo:
+                    LogEntryOutput.Add(logEntry);
+                    break;
+                case ELogMessageType.AdvancedInfo when ShowAdvancedInfo:
+                    LogEntryOutput.Add(logEntry);
+                    break;
+                default:
+                    break;
+            }
         }
     }
 }
