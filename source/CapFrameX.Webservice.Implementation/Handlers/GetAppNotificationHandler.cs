136d49cd3254f29a7024374ad478b36de5c46b9d
Alexander Winnen <alwinnen@gmail.com>
2021-09-25 22:15:25 +0200

    add appnotification endpoint

---
+++ source/CapFrameX.Webservice.Implementation/Handlers/GetAppNotificationHandler.cs
@@ -0,0 +1,28 @@
+﻿using AutoMapper;
+using CapFrameX.Webservice.Data.DTO;
+using CapFrameX.Webservice.Data.Queries;
+using CapFrameX.Webservice.Implementation.Services;
+using MediatR;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CapFrameX.Webservice.Implementation.Handlers
+{
+    public class GetAppNotificationHandler: IRequestHandler<GetAppNotificationQuery, SqAppNotificationDataDTO>
+    {
+        private readonly SquidexService _squidexService;
+        private readonly IMapper _mapper;
+
+        public GetAppNotificationHandler(SquidexService squidexService, IMapper mapper)
+        {
+            _squidexService = squidexService;
+            _mapper = mapper;
+        }
+
+        public async Task<SqAppNotificationDataDTO> Handle(GetAppNotificationQuery request, CancellationToken cancellationToken)
+        {
+            var notification = (await _squidexService.GetAppNotification())?.Data;
+            return notification is null ? null : _mapper.Map<SqAppNotificationDataDTO>(notification);
+        }
+    }
+}
