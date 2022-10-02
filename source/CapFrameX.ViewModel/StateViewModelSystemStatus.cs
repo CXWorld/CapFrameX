cff86a1a9ed6d621e637f9074c147516337016eb
Mikhail Paulyshka <me@mixaill.tk>
2022-10-02 17:51:07 +0300

    View, ViewModel: show D3D ReBar status and BAR size

--- source/CapFrameX.ViewModel/StateViewModelSystemStatus.cs
+++ source/CapFrameX.ViewModel/StateViewModelSystemStatus.cs
@@ -1,20 +1,50 @@
-﻿using CapFrameX.Contracts.Data;
+﻿using System;
+
+using CapFrameX.Contracts.Data;
 
 namespace CapFrameX.ViewModel
 {
     public partial class StateViewModel
     {
-        public bool IsResizableBarSoftwareEnabled => _systemInfo.ResizableBarSoftwareStatus == ESystemInfoTertiaryStatus.Enabled;
+        public bool IsResizableBarD3DEnabled => _systemInfo.ResizableBarD3DStatus == ESystemInfoTertiaryStatus.Enabled;
 
-        public bool IsResizableBarSoftwareStatusValid => _systemInfo.ResizableBarSoftwareStatus != ESystemInfoTertiaryStatus.Error;
+        public bool IsResizableBarD3DStatusValid => _systemInfo.ResizableBarD3DStatus != ESystemInfoTertiaryStatus.Error;
+
+        public double ResizableBarD3DSize => Math.Round(_systemInfo.PciBarSizeD3D / 1024.0 / 1024);
+
+        public bool IsResizableBarVulkanEnabled => _systemInfo.ResizableBarVulkanStatus == ESystemInfoTertiaryStatus.Enabled;
+
+        public bool IsResizableBarVulkanStatusValid => _systemInfo.ResizableBarVulkanStatus != ESystemInfoTertiaryStatus.Error;
+
+        public double ResizableBarVulkanSize => Math.Round(_systemInfo.PciBarSizeVulkan / 1024.0 / 1024);
 
         public bool IsResizableBarHardwareEnabled => _systemInfo.ResizableBarHardwareStatus == ESystemInfoTertiaryStatus.Enabled;
 
         public bool IsResizableBarHardwareStatusValid => _systemInfo.ResizableBarHardwareStatus != ESystemInfoTertiaryStatus.Error;
 
-        public bool IsResizableBarEnabled => IsResizableBarSoftwareEnabled && IsResizableBarHardwareEnabled;
+        public bool IsResizableBarEnabled => (IsResizableBarD3DEnabled || IsResizableBarVulkanEnabled) && IsResizableBarHardwareEnabled;
 
-        public bool IsResizableBarAnyStatusValid => IsResizableBarSoftwareStatusValid || IsResizableBarHardwareStatusValid;
+        public string ResizableBarStatus
+        {
+            get
+            {
+                if (IsResizableBarD3DEnabled && IsResizableBarVulkanEnabled) return "On";
+                if (IsResizableBarD3DEnabled || IsResizableBarVulkanEnabled) return "Partial";
+                return "Off";
+            }
+        }
+
+        public string ResizableBarStatusColor
+        {
+            get
+            {
+                if (IsResizableBarD3DEnabled && IsResizableBarVulkanEnabled) return "LimeGreen";
+                if (IsResizableBarD3DEnabled || IsResizableBarVulkanEnabled) return "Orange";
+                return "OrangeRed";
+            }
+        }
+
+        public bool IsResizableBarAnyStatusValid => IsResizableBarD3DStatusValid || IsResizableBarVulkanStatusValid || IsResizableBarHardwareStatusValid;
 
         public bool IsGameModeEnabled => _systemInfo.GameModeStatus == ESystemInfoTertiaryStatus.Enabled;
 
