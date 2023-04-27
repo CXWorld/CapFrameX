a513cf34d33be24064f6513cbbd1748874b498f1
DevTechProfile <devtechprofile@hotmail.com>
2023-04-27 22:39:27 +0200

    implemented avg PCL (C++ lib)

--- source/CapFrameX.FrameView/FrameViewManager.h
+++ source/CapFrameX.FrameView/FrameViewManager.h
@@ -1,10 +1,8 @@
 #pragma once
-#include <cstdint>
-
 #define FrameView_API __declspec(dllimport)
 
 extern "C" FrameView_API bool IntializeFrameViewSession();
 
-extern "C" FrameView_API void CloseFrameViewSession();
+extern "C" FrameView_API bool CloseFrameViewSession();
 
-extern "C" FrameView_API double GetAveragePcl(const uint32_t pid);
+extern "C" FrameView_API double GetAveragePcl(const unsigned long long pid);
