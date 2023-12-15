6972e35fe3149ba080697b05e5641ed37e30f0b0
DevTechProfile <devtechprofile@hotmail.com>
2023-12-15 18:12:51 +0100

    add Phoenix support

--- source/OpenHardwareMonitor/Hardware/CPU/CPUGroup.cs
+++ source/OpenHardwareMonitor/Hardware/CPU/CPUGroup.cs
@@ -142,7 +142,8 @@ namespace OpenHardwareMonitor.Hardware.CPU
 							case 0x61:
 							case 0x70:
 							case 0x1A:
-								hardware.Add(new AMD17CPU(index, coreThreads, settings, sensorConfig));
+                            case 0x75:
+                                hardware.Add(new AMD17CPU(index, coreThreads, settings, sensorConfig));
 								break;
 							default:
 								hardware.Add(new GenericCPU(index, coreThreads, settings));
