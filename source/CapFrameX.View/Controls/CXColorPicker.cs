a37a2cb0909d0982a2132c12de3ecf0648777cdf
DevTechProfile <DevTechProfile@hotmail.com>
2025-02-16 11:31:07 +0100

    Intel Arc overlay color, game name management exception handling

--- source/CapFrameX.View/Controls/CXColorPicker.cs
+++ source/CapFrameX.View/Controls/CXColorPicker.cs
@@ -15,7 +15,7 @@ namespace CapFrameX.View.Controls
                 new ColorItem(Color.FromRgb(156, 210, 0), "CX Green"),                
                 new ColorItem(Color.FromRgb(200, 0, 0), "Red"),
                 new ColorItem(Color.FromRgb(100, 0, 160), "Purple"),
-                new ColorItem(Color.FromRgb(232, 193, 255), "Intel Pink"),
+                new ColorItem(Color.FromRgb(152, 101, 235), "Intel Arc"),
                 new ColorItem(Color.FromRgb(237, 28, 36), "AMD Red"),
                 new ColorItem(Color.FromRgb(0, 113, 197), "Intel Blue"),
                 new ColorItem(Color.FromRgb(118, 185, 0), "Nvidia Green")
