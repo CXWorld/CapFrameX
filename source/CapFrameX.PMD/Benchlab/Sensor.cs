70da2bdc8f5d662249f0f5e34d83c0dc778990a4
DevTechProfile <devtechprofile@hotmail.com>
2025-04-05 20:00:33 +0200

    starting  implementation BENCHLAB

---
+++ source/CapFrameX.PMD/Benchlab/Sensor.cs
@@ -0,0 +1,43 @@
+﻿namespace CapFrameX.PMD.Benchlab
+{
+    public enum SensorType
+    {
+        Temperature,
+        Humidity,
+        Duty,
+        Revolutions,
+        Voltage,
+        Current,
+        Power,
+        Clock,
+        Usage,
+        Dummy,
+        Other
+    }
+
+    public class Sensor
+    {
+        public Sensor(int id, string shortName, string name, SensorType type)
+        {
+            Id = id;
+            ShortName = shortName;
+            Name = name;
+            Type = type;
+        }
+
+        public Sensor()
+        {
+            Id = 0;
+            ShortName = string.Empty;
+            Name = string.Empty;
+            Type = SensorType.Other;
+        }
+
+        public int Id { get; set; }
+        public string ShortName { get; set; }
+        public string Name { get; set; }
+        public SensorType Type { get; set; }
+        public double Value { get; set; }
+        public bool IsValid { get; set; }
+    }
+}
