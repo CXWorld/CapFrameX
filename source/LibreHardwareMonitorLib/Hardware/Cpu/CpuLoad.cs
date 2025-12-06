536a0e933dc898c35fc8a884f69c0ed311f031cd
DevTechProfile <devtechprofile@hotmail.com>
2025-12-06 18:13:24 +0100

    reworked CpuLoad

--- source/LibreHardwareMonitorLib/Hardware/Cpu/CpuLoad.cs
+++ source/LibreHardwareMonitorLib/Hardware/Cpu/CpuLoad.cs
@@ -4,29 +4,48 @@
 // Partial Copyright (C) Michael Möller <mmoeller@openhardwaremonitor.org> and Contributors.
 // All Rights Reserved.
 
+using CapFrameX.Extensions;
 using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
 using System.Runtime.InteropServices;
-using Windows.Wdk;
-using Windows.Wdk.System.SystemInformation;
-using Windows.Win32.Foundation;
-using Windows.Win32.System.WindowsProgramming;
 
 namespace LibreHardwareMonitor.Hardware.Cpu;
 
 internal class CpuLoad
 {
+    private readonly CpuId[][] _cpuid;
+
     private readonly double[] _threadLoads;
     private long[] _idleTimes;
     private double _totalLoad;
     private long[] _totalTimes;
+    private double _maxLoad;
 
     public CpuLoad(CpuId[][] cpuid)
     {
-        _threadLoads = new double[cpuid.Sum(x => x.Length)];
+        _cpuid = cpuid;
+
+        int threadCount = 0;
+        for (int i = 0; i < cpuid.Length; i++)
+        {
+            threadCount += cpuid[i].Length;
+        }
+
+        _threadLoads = new double[threadCount];
         _totalLoad = 0;
+
+        // check thread indices
+        for (int i = 0; i < cpuid.Length; i++)
+        {
+            for (int j = 0; j < cpuid[i].Length; j++)
+            {
+                long index = cpuid[i][j].Thread;
+
+                if (index >= _threadLoads.Length)
+                {
+                    break;
+                }
+            }
+        }
 
         try
         {
@@ -42,117 +61,44 @@ internal class CpuLoad
             IsAvailable = true;
     }
 
-    public bool IsAvailable { get; }
-
     private static bool GetTimes(out long[] idle, out long[] total)
-    {
-        return !Software.OperatingSystem.IsUnix ? GetWindowsTimes(out idle, out total) : GetUnixTimes(out idle, out total);
-    }
-
-    private static unsafe bool GetWindowsTimes(out long[] idle, out long[] total)
     {
         idle = null;
         total = null;
 
-        int size = sizeof(SYSTEM_PROCESSOR_PERFORMANCE_INFORMATION) * 64;
-        uint returnSize = 0;
-        IntPtr handle = Marshal.AllocHGlobal(size);
-        SYSTEM_PROCESSOR_PERFORMANCE_INFORMATION[] perfInformation;
-
-        while (true)
+        //Query processor idle information
+        Interop.NtDll.SYSTEM_PROCESSOR_IDLE_INFORMATION[] idleInformation = new Interop.NtDll.SYSTEM_PROCESSOR_IDLE_INFORMATION[64];
+        int idleSize = Marshal.SizeOf(typeof(Interop.NtDll.SYSTEM_PROCESSOR_IDLE_INFORMATION));
+        if (Interop.NtDll.NtQuerySystemInformation(Interop.NtDll.SYSTEM_INFORMATION_CLASS.SystemProcessorIdleInformation, idleInformation, idleInformation.Length * idleSize, out int idleReturn) != 0)
         {
-            //Query processor performance information
-            NTSTATUS status = PInvoke.NtQuerySystemInformation(SYSTEM_INFORMATION_CLASS.SystemProcessorPerformanceInformation, (void*)handle, (uint)size, &returnSize);
-            if (status == NTSTATUS.STATUS_INFO_LENGTH_MISMATCH)
-            {
-                size = (int)returnSize;
-                handle = Marshal.ReAllocHGlobal(handle, new IntPtr(size));
-            }
-            else
-            {
-                if (status != NTSTATUS.STATUS_SUCCESS)
-                {
-                    Marshal.FreeHGlobal(handle);
-                    return false;
-                }
-
-                SYSTEM_PROCESSOR_PERFORMANCE_INFORMATION* perfInformationPtr = (SYSTEM_PROCESSOR_PERFORMANCE_INFORMATION*)handle;
-
-                int perfItemsCount = (int)(returnSize / sizeof(SYSTEM_PROCESSOR_PERFORMANCE_INFORMATION));
-                perfInformation = new SYSTEM_PROCESSOR_PERFORMANCE_INFORMATION[perfItemsCount];
-                for (int i = 0; i < perfItemsCount; i++)
-                {
-                    perfInformation[i] = perfInformationPtr[i];
-                }
-
-                Marshal.FreeHGlobal(handle);
-                break;
-            }
+            return false;
         }
 
-        idle = new long[perfInformation.Length];
-        total = new long[perfInformation.Length];
+        //Query processor performance information
+        Interop.NtDll.SYSTEM_PROCESSOR_PERFORMANCE_INFORMATION[] perfInformation = new Interop.NtDll.SYSTEM_PROCESSOR_PERFORMANCE_INFORMATION[64];
+        int perfSize = Marshal.SizeOf(typeof(Interop.NtDll.SYSTEM_PROCESSOR_PERFORMANCE_INFORMATION));
+        if (Interop.NtDll.NtQuerySystemInformation(Interop.NtDll.SYSTEM_INFORMATION_CLASS.SystemProcessorPerformanceInformation, perfInformation, perfInformation.Length * perfSize, out int perfReturn) != 0)
+        {
+            return false;
+        }
+
+        idle = new long[idleReturn / idleSize];
+        for (int i = 0; i < idle.Length; i++)
+        {
+            idle[i] = idleInformation[i].IdleTime;
+        }
+
+        total = new long[perfReturn / perfSize];
         for (int i = 0; i < total.Length; i++)
         {
-            idle[i] = perfInformation[i].IdleTime;
             total[i] = perfInformation[i].KernelTime + perfInformation[i].UserTime;
         }
 
         return true;
     }
 
-    private static bool GetUnixTimes(out long[] idle, out long[] total)
-    {
-        idle = null;
-        total = null;
+    public bool IsAvailable { get; }
 
-        List<long> idleList = [];
-        List<long> totalList = [];
-
-        if (!File.Exists("/proc/stat"))
-            return false;
-
-        string[] cpuInfos = File.ReadAllLines("/proc/stat");
-
-        // currently parse the OverAll CPU info
-        // cpu   1583083 737    452845   36226266 723316   63685 31896     0       0       0
-        // cpu0  397468  189    109728   9040007  191429   16939 14954     0       0       0
-        // 0=cpu 1=user  2=nice 3=system 4=idle   5=iowait 6=irq 7=softirq 8=steal 9=guest 10=guest_nice
-        foreach (string cpuInfo in cpuInfos.Where(s => s.StartsWith("cpu") && s.Length > 3 && s[3] != ' '))
-        {
-            string[] overall = cpuInfo.Split([' '], StringSplitOptions.RemoveEmptyEntries);
-
-            try
-            {
-                // Parse idle information.
-                idleList.Add(long.Parse(overall[4]));
-            }
-            catch
-            {
-                // Ignored.
-            }
-
-            // Parse total information = user + nice + system + idle + iowait + irq + softirq + steal + guest + guest_nice.
-            long currentTotal = 0;
-            foreach (string item in overall.Skip(1))
-            {
-                try
-                {
-                    currentTotal += long.Parse(item);
-                }
-                catch
-                {
-                    // Ignored.
-                }
-            }
-
-            totalList.Add(currentTotal);
-        }
-
-        idle = idleList.ToArray();
-        total = totalList.ToArray();
-        return true;
-    }
 
     public double GetTotalLoad()
     {
@@ -161,51 +107,86 @@ internal class CpuLoad
 
     public double GetThreadLoad(int thread)
     {
-        return _threadLoads[thread];
+        double threadLoad = 0.0;
+        if (thread < _threadLoads.Length)
+        {
+            threadLoad = _threadLoads[thread];
+        }
+
+        return threadLoad;
     }
 
     public void Update()
     {
-        if (_idleTimes == null || !GetTimes(out long[] newIdleTimes, out long[] newTotalTimes))
+        if (_idleTimes == null)
             return;
 
-        int minDiff = Software.OperatingSystem.IsUnix ? 100 : 100000;
-        for (int i = 0; i < Math.Min(newTotalTimes.Length, _totalTimes.Length); i++)
+        try
         {
-            if (newTotalTimes[i] - _totalTimes[i] < minDiff)
+            if (!GetTimes(out long[] newIdleTimes, out long[] newTotalTimes))
                 return;
+
+            if (newIdleTimes.IsNullOrEmpty() || newTotalTimes.IsNullOrEmpty())
+                return;
+
+            for (int i = 0; i < Math.Min(newTotalTimes.Length, _totalTimes.Length); i++)
+                if (newTotalTimes[i] - _totalTimes[i] < 100000)
+                    return;
+
+            double total = 0;
+            _maxLoad = 0;
+            int count = 0;
+            for (int i = 0; i < _cpuid.Length; i++)
+            {
+                for (int j = 0; j < _cpuid[i].Length; j++)
+                {
+                    double value = 0.0;
+                    long index = _cpuid[i][j].Thread;
+                    if (index < newIdleTimes.Length && index < _totalTimes.Length)
+                    {
+                        double idle = 0.0;
+                        if (index < newIdleTimes.Length && index < newTotalTimes.Length)
+                        {
+                            idle = (double)(newIdleTimes[index] - _idleTimes[index]) /
+                                (newTotalTimes[index] - _totalTimes[index]);
+                        }
+
+                        value = idle;
+                        total += idle;
+                        count++;
+                    }
+                    value = 1.0 - value;
+                    value = value < 0 ? 0 : value;
+
+                    if (index < _threadLoads.Length)
+                    {
+                        _threadLoads[index] = value * 100;
+
+                        if (_threadLoads[index] > _maxLoad)
+                            _maxLoad = _threadLoads[index];
+                    }
+                }
+            }
+
+            if (count > 0)
+            {
+                total = 1.0 - total / (double)count;
+                total = total < 0 ? 0 : total;
+            }
+            else
+            {
+                total = 0;
+            }
+
+            _totalLoad = total * 100.0;
+            _totalTimes = newTotalTimes;
+            _idleTimes = newIdleTimes;
         }
-
-        if (newIdleTimes == null)
-            return;
-
-        double total = 0;
-        int count = 0;
-        for (int i = 0; i < _threadLoads.Length && i < _idleTimes.Length && i < newIdleTimes.Length; i++)
+        catch
         {
-            double idle = (newIdleTimes[i] - _idleTimes[i]) / (double)(newTotalTimes[i] - _totalTimes[i]);
-            idle = idle < 0.0 ? 0.0 : idle;
-            idle = idle > 1.0 ? 1.0 : idle;
-
-            double load = 100.0 * (1.0 - Math.Min(idle, 1.0));
-            _threadLoads[i] = Math.Round(load, 2);
-            total += idle;
-            count++;
+            _totalLoad = 0.0;
+            _totalTimes = new long[Environment.ProcessorCount]; ;
+            _idleTimes = new long[Environment.ProcessorCount]; ;
         }
-
-        if (count > 0)
-        {
-            total = 1.0 - (total / count);
-            total = total < 0.0 ? 0.0 : total;
-            total = total > 1.0 ? 1.0 : total;
-        }
-        else
-        {
-            total = 0;
-        }
-
-        _totalLoad = Math.Round(total * 100.0, 2);
-        _totalTimes = newTotalTimes;
-        _idleTimes = newIdleTimes;
     }
 }
---
+++ source/LibreHardwareMonitorLib/Interop/NtDll.cs
@@ -0,0 +1,45 @@
+﻿using System.Runtime.InteropServices;
+
+namespace LibreHardwareMonitor.Interop
+{
+    internal class NtDll
+    {
+        [StructLayout(LayoutKind.Sequential)]
+        internal struct SYSTEM_PROCESSOR_PERFORMANCE_INFORMATION
+        {
+            public long IdleTime;
+            public long KernelTime;
+            public long UserTime;
+            public long DpcTime;
+            public long InterruptTime;
+            public uint InterruptCount;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        internal struct SYSTEM_PROCESSOR_IDLE_INFORMATION
+        {
+            public long IdleTime;
+            public long C1Time;
+            public long C2Time;
+            public long C3Time;
+            public uint C1Transitions;
+            public uint C2Transitions;
+            public uint C3Transitions;
+            public uint Padding;
+        }
+
+        internal enum SYSTEM_INFORMATION_CLASS
+        {
+            SystemProcessorPerformanceInformation = 8,
+            SystemProcessorIdleInformation = 42
+        }
+
+        [DllImport("ntdll.dll")]
+        [DefaultDllImportSearchPaths(DllImportSearchPath.System32)]
+        internal static extern int NtQuerySystemInformation(SYSTEM_INFORMATION_CLASS SystemInformationClass, [Out] SYSTEM_PROCESSOR_PERFORMANCE_INFORMATION[] SystemInformation, int SystemInformationLength, out int ReturnLength);
+
+        [DllImport("ntdll.dll")]
+        [DefaultDllImportSearchPaths(DllImportSearchPath.System32)]
+        internal static extern int NtQuerySystemInformation(SYSTEM_INFORMATION_CLASS SystemInformationClass, [Out] SYSTEM_PROCESSOR_IDLE_INFORMATION[] SystemInformation, int SystemInformationLength, out int ReturnLength);
+    }
+}
