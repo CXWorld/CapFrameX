96d2e54d455afee358faac005556f836e3e9535c
DevTechProfile <DevTechProfile@hotmail.com>
2026-01-24 09:03:25 +0100

    removed certificate, version

--- source/CapFrameX/export-cert.ps1
+++
@@ -1,3 +0,0 @@
-$cert = Get-ChildItem -Path 'Cert:\CurrentUser\My' | Where-Object { $_.Subject -eq 'CN=CapFrameX Development' } | Select-Object -First 1
-$password = ConvertTo-SecureString -String 'CapFrameXDev123' -Force -AsPlainText
-Export-PfxCertificate -Cert $cert -FilePath 'D:\Projekte\Source\Example Code\CapFrameX\source\CapFrameX\CapFrameX.pfx' -Password $password
